0d16ad2ff42d08a5e5c3572d32dd6ac5fea1e098
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-02-17 17:29:25 +0100

    Backup commit.

--- modules/ProvVoipEnvia/Database/Migrations/2016_01_26_000001_create_EnviaOrder_table.php
+++ modules/ProvVoipEnvia/Database/Migrations/2016_01_26_000001_create_EnviaOrder_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateEnviaOrdersTable extends BaseMigration {
+class CreateEnviaOrderTable extends BaseMigration {
 
 	// name of the table to create
 	protected $tablename = "enviaorder";
