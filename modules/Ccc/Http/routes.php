41e162aced764201366c7318f153bce5c0de5568
Torsten Schmidt <schmto@roetzer-engineering.com>
2016-05-18 09:31:06 +0200

    CCC: auth - adapt to L5.2 and enable throttling

--- modules/Ccc/Http/routes.php
+++ modules/Ccc/Http/routes.php
@@ -6,14 +6,14 @@ Route::get('ccc', array('as' => 'Home', 'uses' => 'Modules\Ccc\Http\Controllers\
 
 
 // Auth => login form
-Route::get('ccc/auth/login', array('as' => 'CccAuth.login', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@showLogin'));
+Route::get('ccc/auth/login', array('as' => 'CccAuth.login', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@showLoginForm'));
 
 // Auth => process form data
-Route::post('ccc/auth/login', array('as' => 'CccAuth.login', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@doLogin'));
+Route::post('ccc/auth/login', array('as' => 'CccAuth.login', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@postLogin'));
 
 // Auth => Logout
-Route::get ('ccc/auth/logout', array('as' => 'CccAuth.logout', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@doLogout'));
-Route::post('ccc/auth/logout', array('as' => 'CccAuth.logout', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@doLogout'));
+Route::get ('ccc/auth/logout', array('as' => 'CccAuth.logout', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@getLogout'));
+Route::post('ccc/auth/logout', array('as' => 'CccAuth.logout', 'uses' => 'Modules\Ccc\Http\Controllers\AuthController@getLogout'));
 
 
 // CCC internal stuff, with CCC authentication checking
