2d19975ad4ec9ca3cb5d60c599d19042880606fd
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-06-01 16:13:14 +0200

    Minimal change.

--- modules/ProvVoipEnvia/Http/routes.php
+++ modules/ProvVoipEnvia/Http/routes.php
@@ -6,7 +6,8 @@ BaseRoute::group([], function() {
 	BaseRoute::get('/provvoipenvia/request/{job}', array('as' => 'ProvVoipEnvia.request', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@request'));
 	BaseRoute::get('/provvoipenvia/cron/{job}', array('as' => 'ProvVoipEnvia.cron', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@cron'));
 
+	BaseRoute::get('/enviaorderdocument/{id}/show', array('as' => 'EnviaOrderDocument.show', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\EnviaOrderDocumentController@show'));
+
 	BaseRoute::resource('EnviaOrder', 'Modules\ProvVoipEnvia\Http\Controllers\EnviaOrderController');
-	BaseRoute::resource('EnviaOrderDocument', 'Modules\ProvVoipEnvia\Http\Controllers\EnviaOrderDocumentController',
-		['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy', 'show']]);
+	BaseRoute::resource('EnviaOrderDocument', 'Modules\ProvVoipEnvia\Http\Controllers\EnviaOrderDocumentController');
 });
