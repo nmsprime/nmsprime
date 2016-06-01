302c28b9dd0a32dd2e9f6f29e28042636408f859
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-06-01 17:25:19 +0200

    ProvVoipEnvia@cron route is without authentification.

--- modules/ProvVoipEnvia/Http/routes.php
+++ modules/ProvVoipEnvia/Http/routes.php
@@ -1,10 +1,11 @@
 <?php
 
+Route::get('/provvoipenvia/cron/{job}', array('as' => 'ProvVoipEnvia.cron', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@cron'));
+
 BaseRoute::group([], function() {
 
 	BaseRoute::get('/provvoipenvia/index', array('as' => 'ProvVoipEnvia.index', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@index'));
 	BaseRoute::get('/provvoipenvia/request/{job}', array('as' => 'ProvVoipEnvia.request', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@request'));
-	BaseRoute::get('/provvoipenvia/cron/{job}', array('as' => 'ProvVoipEnvia.cron', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController@cron'));
 
 	BaseRoute::get('/enviaorderdocument/{id}/show', array('as' => 'EnviaOrderDocument.show', 'uses' => 'Modules\ProvVoipEnvia\Http\Controllers\EnviaOrderDocumentController@show'));
 
