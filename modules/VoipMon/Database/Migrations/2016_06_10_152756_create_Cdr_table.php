f95b0bb34a7f1b7c85acca292f6ba994aecd31b4
Ole Ernst <olebowle@gmx.com>
2016-06-13 14:08:21 +0200

    LAR87: adapt cdr migration to use external database

--- modules/VoipMon/Database/Migrations/2016_06_10_152756_create_Cdr_table.php
+++ modules/VoipMon/Database/Migrations/2016_06_10_152756_create_Cdr_table.php
@@ -3,31 +3,42 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCdrTable extends Migration {
+class CreateCdrTable extends BaseMigration {
 
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('Cdr', function(Blueprint $table)
-        {
-            $table->increments('id');
+	// Default config of the voipmonitor daemon is to create its own database, use it instead of db_lara
+	protected $connection = 'mysql-voipmonitor';
+	// name of the table to create
+	protected $tablename = 'cdr';
 
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Let voipmonitor create the basic database for us
+		system('voipmonitor --config-file /etc/voipmonitor.conf --update-schema');
+		// We just append the column we need
+		Schema::connection($this->connection)->table($this->tablename, function(Blueprint $table)
+		{
+			// We can't use up_table_generic($table), as existing table already contains an id column
+			$table->timestamps();
+			$table->softDeletes();
+			$table->integer('phonenumber_id')->unsigned()->nullable();
+		});
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('Cdr');
-    }
+		return parent::up();
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::connection($this->connection)->drop($this->tablename);
+	}
 
 }
