986e6faa60dc84e56085a2c93395b14f634710d2
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-07-05 19:10:09 +0200

    Added explanatory text.

--- modules/ProvVoipEnvia/Resources/views/EnviaOrder/edit.blade.php
+++ modules/ProvVoipEnvia/Resources/views/EnviaOrder/edit.blade.php
@@ -9,6 +9,10 @@
 
 			if ($additional_data['user_actions']) {
 				echo '<div class="col-md-12" style="margin-top: 30px; padding-top: 20px; border-top:solid #888 1px">';
+
+				echo '<h5>EnviaOrder has been updated</h5>';
+				echo 'Please check if user interaction is necessary<br><br>';
+
 				$tmp = array();
 				foreach ($additional_data['user_actions'] as $linktext => $link) {
 					array_push($tmp, '<a href="'.$link.'" target="_self">» '.$linktext.'</a>');
