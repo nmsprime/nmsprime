99c66497c0e12e0cd580471c3eb6399cc5df93c2
Ole Ernst <olebowle@gmx.com>
2016-07-11 13:26:40 +0200

    LAR85: fix copy+paste typo

--- modules/HfcCustomer/Http/routes.php
+++ modules/HfcCustomer/Http/routes.php
@@ -12,5 +12,5 @@ BaseRoute::group([], function() {
 });
 
 Route::group(['middleware' => 'auth:view', 'prefix' => 'app/data/hfccustomer'], function () {
-	Route::get('kml/{filename}', array('uses' => 'Modules\HfcBase\Http\Controllers\HfcCustomerController@get_file'));
+	Route::get('kml/{filename}', array('uses' => 'Modules\HfcCustomer\Http\Controllers\HfcCustomerController@get_file'));
 });
