698d15457801ac806c899a7d9f8f6a589baad851
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2016-08-16 16:27:05 +0200

    Added workaround for currently defective EnviaAPI.

--- modules/ProvVoipEnvia/Entities/ProvVoipEnvia.php
+++ modules/ProvVoipEnvia/Entities/ProvVoipEnvia.php
@@ -1255,6 +1255,16 @@ class ProvVoipEnvia extends \BaseModel {
 		foreach ($defaults as $xml_field => $payload) {
 			$inner_xml->addChild($xml_field, $payload);
 		}
+
+// workaround for defective EnviaAPI – delete this if running normal again!!
+// BEGIN OF WORKAROUND
+$contract_configurations = $inner_xml->addChild('contract_configurations');
+$contract_configuration_data = $contract_configurations->addChild('contract_configuration_data');
+$type = $contract_configuration_data->addChild('type', 'sprachtarif');
+$articles = $contract_configuration_data->addChild('articles');
+$articles->addChild('article_id', '399554');
+// END OF WORKAROUND
+
 	}
 
 
