e176c7dba7176ef8d164b868d97ba2c14cbd0605
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2016-10-11 15:37:02 +0200

    Bugfix-Item: Properly set accounting text (if empty no hyphen)

--- modules/BillingBase/Entities/Item.php
+++ modules/BillingBase/Entities/Item.php
@@ -387,7 +387,8 @@ class Item extends \BaseModel {
 
 		$this->charge = $this->product->type == 'Credit' ?  (-1) * $this->credit_amount : $this->product->price * $ratio * $this->count;
 		$this->ratio  = $ratio ? $ratio : 1;
-		$this->invoice_description = $this->product->name.' '.$text.' - '.$this->accounting_text;
+		$this->invoice_description = $this->product->name.' '.$text;
+		$this->invoice_description .= $this->accounting_text ? ' - '.$this->accounting_text : '';
 
 		return true;
 	}
