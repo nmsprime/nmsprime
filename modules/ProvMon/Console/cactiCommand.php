1e36bae9d390a1a3eb5fd0a4f165b71e9cf3d68b
Ole Ernst <olebowle@gmx.com>
2016-12-08 11:31:16 +0100

    add missing devices (shouldn't happend) to cacti once a day, make adding devices more robust

--- modules/ProvMon/Console/cactiCommand.php
+++ modules/ProvMon/Console/cactiCommand.php
@@ -163,7 +163,7 @@ class cactiCommand extends Command {
 				->where('name', '=', 'cmts')
 				->select('id')->first()->id;
 
-			$out = system("php -q $path/add_device.php --description=$name --ip=$hostname --template=$host_template_id --community=$community --avail=snmp --version=2");
+			$out = system("php -q $path/add_device.php --description=\"$name\" --ip=$hostname --template=$host_template_id --community=\"$community\" --avail=snmp --version=2");
 			preg_match('/^Success - new device-id: \(([0-9]+)\)$/', $out, $matches);
 			if(count($matches) != 2)
 				continue;
