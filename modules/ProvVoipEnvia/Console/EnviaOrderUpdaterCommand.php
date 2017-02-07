66260d3750ae169ef1f0d4f703c34aa7f477d594
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-02-07 17:52:28 +0100

    Moved method _perform_curl_request to trait.
    This is used in several console command classes.
    
    Maybe there is more that can be moved?

--- modules/ProvVoipEnvia/Console/EnviaOrderUpdaterCommand.php
+++ modules/ProvVoipEnvia/Console/EnviaOrderUpdaterCommand.php
@@ -14,6 +14,9 @@ use \Modules\ProvVoipEnvia\Http\Controllers\ProvVoipEnviaController;
  */
 class EnviaOrderUpdaterCommand extends Command {
 
+	// get some methods used by several updaters
+	use \App\Console\Commands\DatabaseUpdaterTrait;
+
 	/**
 	 * The console command name.
 	 */
@@ -129,39 +132,6 @@ class EnviaOrderUpdaterCommand extends Command {
 
 	}
 
-	/**
-	 * Update an order (using a curl request against the given URL.
-	 * Since updating uses the same functionality as updating via frontend we accessing the cron method in ProvVoipEnviaController using cURL.
-	 *
-	 * This may be not the best way – but the one without bigger refactoring of the sources…
-	 * TODO: Evaluate other solutions…
-	 *
-	 * @author Patrick Reichel
-	 *
-	 * @param $url URL to be accessed by cURL
-	 */
-	protected function _perform_curl_request($url) {
-
-		$ch = curl_init();
-
-		$opts = array(
-			CURLOPT_URL => $url,
-			CURLOPT_HEADER => false,
-			CURLOPT_SSL_VERIFYPEER => false,	// no valid cert for “localhost” – so we don't check
-			CURLOPT_RETURNTRANSFER => TRUE,		// return result instead of instantly printing to screen
-		);
-
-		curl_setopt_array($ch, $opts);
-
-		$res = curl_exec($ch);
-		$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-		if ($http_code != 200) {
-			Log::error("HTTP error ".$http_code." occured in scheduled updating of envia orders calling ".$url);
-		}
-
-		curl_close($ch);
-	}
 
 	/**
 	 * Check if the given order id has been updated.
