700313fafeb36ef0652075ba1f9951ca9616b58f
Patrick Reichel <patrick.reichel@roetzer-engineering.com>
2017-05-10 10:13:05 +0200

    Added new database table enviacontract.

--- modules/ProvVoipEnvia/Database/Migrations/2017_05_09_000100_create_EnviaContract_table.php
+++ modules/ProvVoipEnvia/Database/Migrations/2017_05_09_000100_create_EnviaContract_table.php
@@ -21,16 +21,38 @@ class CreateEnviaContractTable extends BaseMigration {
 			$this->up_table_generic($table);
 
 
-			$table->date('creation_date');
-			$table->date('termination_date')->nullable()->default(NULL);
-			$table->string('customer_reference', 60)->nullable()->default(NULL);
-			$table->string('contract_reference', 60);
-			$table->string('previous_contract_reference', 60)->nullable()->default(NULL);;
-			$table->date('installation_address_change_date')->nullable()->default(NULL);
-			$table->integer('contract_id');
-			$table->integer('modem_id');
+			$table->date('external_creation_date')->nullable()->default(NULL);
+			$table->date('external_termination_date')->nullable()->default(NULL);
+			$table->string('envia_customer_reference', 60)->nullable()->default(NULL);
+			$table->string('envia_contract_reference', 60)->nullable()->default(NULL);
+			$table->date('end_date')->nullable()->default(NULL);
+			$table->date('start_date')->nullable()->default(NULL);
+
+			// Envia contract reference can change over the time (e.g. on contract_relocate)
+			$table->integer('next_id')->unsigned()->nullable()->default(NULL);
+			$table->integer('prev_id')->unsigned()->nullable()->default(NULL);
+			$table->string('end_reason', 60)->nullable()->default(NULL); // API method that ends this contract
+
+			// this are some contract related configuration fields ⇒ watch API description for details
+			$table->integer('lock_level')->unsigned()->nullable()->default(NULL);
+			$table->enum('method', array('MGCP', 'SIP'))->default('SIP');	// ATM there is only SIP implemented
+			$table->integer('sla_id')->unsigned()->nullable()->default(NULL);
+			$table->integer('tariff_id')->unsigned()->nullable()->default(NULL);
+			$table->integer('variation_id')->unsigned()->nullable()->default(NULL);
+
+			// relations of this envia contract
+			// they are also related to
+			//	phonenumbers ⇒ this will be stored as enviacontract_id in phonenumbermanagement
+			//	enviaorders ⇒ this will be stored as enviacontract_id in enviaorder
+			$table->integer('contract_id')->unsigned()->nullable()->default(NULL);
+			$table->integer('modem_id')->unsigned()->nullable()->default(NULL);
 
 		});
+
+		$this->set_fim_fields([
+			'envia_customer_reference',
+			'envia_contract_reference',
+		]);
 
 		return parent::up();
 	}
