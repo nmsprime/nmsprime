791088ccafd73b8bcb397f7eeecf94580035005e
Christian Schramm <christian.schramm@roetzer-engineering.com>
2017-05-17 13:14:30 +0200

    Prettify datatables and put them into Container for even better responsivenes

--- modules/ProvMon/Resources/views/analyses.blade.php
+++ modules/ProvMon/Resources/views/analyses.blade.php
@@ -120,40 +120,75 @@
 
 
 @section('content_realtime')
-	@if ($realtime)
-	
-	<font color="green"><b>{{$realtime['forecast']}}</b></font><br>
+	<?php $realtime=array(
+		"measure" => [ "System" =>
+					[ "SysDescr"	=> [0 => "Thomson PacketCable E-MTA <<HW_REV: 1.0.; VENDOR: Thomson; BOOTR: 2.1.7dm; SW_REV: ST8B.01.45; MODEL: THG540>>"],
+      				"Firmware"		=> [0 => "ST8B.01.45"],
+      				"Uptime"		=> [0 => "116 Days 1 Hours 24 Min 13 Sec"],
+      				"DOCSIS"		=> [0 => "DOCSIS 2.0"]
+      				],
+      			"Downstream" =>
+      				["Frequency MHz" => [0 => 338, 			1 => 339, 		2 => 340],
+      				"Modulation" 	=> [0 => "QAM256", 		1 => "QAM256", 	2 => "QAM256"],
+      				"Power dBmV"	=> [0 => 5.90, 			1 => 5.9, 		2 => 5.9 ],
+      				"MER dB" 		=> [0 => 39.8, 			1 => 39.8, 		2 => 40.9 ],
+      				"Microreflection -dBc" =>  [0 => "30", 	1 => "30", 		2 => "30"],
+      				"Operational CHs %" => [0 => 100, 		1 => 100, 		2 => 100]
+    				],
+  				"Upstream" =>
+  					["Frequency MHz" => [0 => 25.4, 			1 => 25.6 ],
+  					"Power dBmV" 	=> [0 => 35.2, 				1 => 35.1 ],
+      				"Width MHz" 	=> [0 => 6.4, 				1 => 6.5 ],
+      				"Modulation Profile" => [0 => "0",			1 => "0"],
+      				"SNR dB" 		=> [0 => 36.1,				1 => 30.1],
+      				"Operational CHs %" => [0 => 100,			1 => 100]
+    				],
+    			"CMTS" =>
+    				[ "Hostname" 	=> [0 => "dev-cable-gw01"]
+    				]
+  				],
+  		"forecast" => "TODO"
+	); ?>
 
 	@foreach ($realtime['measure'] as $tablename => $table)
-		<h5>{{$tablename}}</h5>
-		<table class="table datatable table-bordered table-striped responsive" width="100%">
+		<h4>{{$tablename}}</h4>
 			@if ($tablename == "Downstream" || $tablename == "Upstream"  )
-			<tr>
-				<th>
-					#
-				</th>
-				@foreach ($table as $colheader => $colarray)
-				<th width="120px">
-					{{$colheader}}
-				</th>
-			@endforeach
-			</tr>
-				<?php $max = count(current($table)); ?>
-				@for ($i = 0; $i < $max ; $i++)
-					<tr>
-						<td> {{ $i }}</td>
-						@foreach ($table as $colheader => $colarray)
-							<td> <font color="grey"> {{ htmlspecialchars( $colarray[$i] ) }} </font> </td>
-						@endforeach
-					</tr>
-				@endfor
+			<div class="table-responsive">
+				<table class="table streamtable table-bordered table-hover" width="100%">
+					<thead>
+						<tr class="active">
+							<th> </th>
+							<th>#</th>
+							@foreach ($table as $colheader => $colarray)
+								@if ($colheader != "Operational CHs %")
+								<th>{{$colheader}}</th>
+								@endif
+							@endforeach
+						</tr>
+					</thead>
+					<tbody>
+						<?php $max = count(current($table)); ?>
+						@for ($i = 0; $i < $max ; $i++)
+						<tr>
+							<td width="20"></td>
+							<td width="20"> {{ $i+1 }}</td>
+							@foreach ($table as $colheader => $colarray)
+								@if ($colheader != "Operational CHs %")
+								<td align="center"> <font color="grey"> {{ htmlspecialchars( $colarray[$i] ) }} </font> </td>
+								@endif
+							@endforeach
+						</tr>
+						@endfor
+					</tbody>
+				</table>
+			</div>
 			@else
+			<table class="table">
 			@foreach ($table as $rowname => $row)
 				<tr>
-					<th width="120px">
+					<th width="15%">
 						{{$rowname}}
 					</th>
-
 					@foreach ($row as $linename => $line)
 						<td>
 							<font color="grey">{{htmlspecialchars($line)}}</font>
@@ -161,11 +196,7 @@
 					@endforeach
 				</tr>
 			@endforeach
+			</table>
 			@endif
-		</table>
 	@endforeach
-
-	@else
-		<font color="red">{{trans('messages.modem_offline')}}</font>
-	@endif
 @stop
