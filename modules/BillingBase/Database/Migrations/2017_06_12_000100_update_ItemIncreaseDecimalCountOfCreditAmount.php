33403252a244e9b41d5f8a061a4a073621e67486
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2017-06-12 12:14:18 +0200

    Billing: change decimal count of Items Credit Amount to 4, fix edit view helper message and translation

---
+++ modules/BillingBase/Database/Migrations/2017_06_12_000100_update_ItemIncreaseDecimalCountOfCreditAmount.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateItemIncreaseDecimalCountOfCreditAmount extends BaseMigration {
+
+	// name of the table to create
+	protected $tablename = "item";
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table($this->tablename, function(Blueprint $table)
+		{
+			$table->decimal('credit_amount', 9, 4)->nullable()->change();
+		});
+
+		return parent::up();
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// reverting the changes doesn't make sense
+	}
+
+}
