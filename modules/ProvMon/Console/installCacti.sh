35b2566e8faea6c549b0b1a293ae3827ccbe8cf2
Ole Ernst <ole.ernst@roetzer-engineering.com>
2017-11-03 16:46:09 +0100

    let cacti have access to the .env file

--- modules/ProvMon/Console/installCacti.sh
+++ modules/ProvMon/Console/installCacti.sh
@@ -46,6 +46,9 @@ su -s /bin/bash -c "php import_template.php --filename=/var/www/nmsprime/modules
 su -s /bin/bash -c "php import_template.php --filename=/var/www/nmsprime/modules/ProvMon/Console/cacti/cacti_host_template_casa_cmts.xml" apache
 su -s /bin/bash -c "php import_template.php --filename=/var/www/nmsprime/modules/ProvMon/Console/cacti/cacti_host_template_cisco_cmts.xml" apache
 
+# add cacti to group apache, so it is able to read the .env file
+gpasswd -a cacti apache
+
 php /var/www/nmsprime/artisan view:clear
 # we call ProvMonController from cacti and thus need to be able to write to the following folder
 chmod o+w /var/www/nmsprime/storage/framework/views
