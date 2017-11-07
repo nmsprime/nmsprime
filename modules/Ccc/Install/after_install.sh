23984fb81affa05a9d19ffe3d05454ba83d5eb65
Torsten Schmidt <schmto@roetzer-engineering.com>
2017-11-07 10:03:04 +0100

    Install: CCC - add firewalld settings

--- modules/Ccc/Install/after_install.sh
+++ modules/Ccc/Install/after_install.sh
@@ -5,6 +5,10 @@ mysql -u root -e "CREATE DATABASE nmsprime_ccc;"
 mysql -u root -e "GRANT ALL ON nmsprime_ccc.* TO 'nmsprime_ccc'@'localhost' IDENTIFIED BY '$ccc_pw'";
 sed -i "s/^CCC_DB_PASSWORD=$/CCC_DB_PASSWORD=$ccc_pw/" "$env"
 
+# firewalld - enable admin interface
+firewall-cmd --add-service=https --zone=public --permanent
+firewall-cmd --reload
+
 # reload apache config
 systemctl reload httpd
 
