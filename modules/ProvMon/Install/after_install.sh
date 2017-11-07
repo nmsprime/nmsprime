21642bf8cd44105dc804bfbf647fa3410b471105
Torsten Schmidt <schmto@roetzer-engineering.com>
2017-11-07 16:22:54 +0100

    Install: cacti - fix mysql populate cacti default tbls

--- modules/ProvMon/Install/after_install.sh
+++ modules/ProvMon/Install/after_install.sh
@@ -20,9 +20,13 @@ mysql -u root -e "GRANT ALL ON cacti.* TO 'cactiuser'@'localhost' IDENTIFIED BY 
 # populate timezone info
 mysql -u root -e "GRANT SELECT ON mysql.time_zone_name TO 'cactiuser'@'localhost';";
 
-# populate default DB
+# set psw in cacti db config file
 sed -i "s/database_password =.*/database_password = \"$mysql_cacti_psw\";/" /etc/cacti/db.php
-mysql cacti -u cactiuser --password="$mysql_cacti_psw" < /usr/share/doc/cacti-*/cacti.sql
+
+# populate default DB
+# NOTE: for some unknown reasons, doing this in one line, like "mysql ... < ", does not work. maybe due two special char * in file link
+cacti_file=`ls /usr/share/doc/cacti-*/cacti.sql`
+mysql -u cactiuser --password="$mysql_cacti_psw" cacti < "$cacti_file"
 
 # allow guest user to access graphs without login (also invalidate its password, by setting an imposible bcrypt hash)
 # send SNMP queries concurrenly to modems (depending on no of cpus)
