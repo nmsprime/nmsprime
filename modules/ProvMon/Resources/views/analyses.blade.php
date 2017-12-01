304afd8b63925aae4001f13501340216cde4e505
Christian Schramm <christian.schramm@roetzer-engineering.com>
2017-12-01 10:53:08 +0100

    Cleanup Footer and move specific parts to the corresponding files     -> datatables should only be in code when used     -> start refactoring Blades and cleanup old stuff

--- modules/ProvMon/Resources/views/analyses.blade.php
+++ modules/ProvMon/Resources/views/analyses.blade.php
@@ -271,4 +271,51 @@
 		<?php endif; ?>
 	</script>
 
+	<script language="javascript">
+		$(document).ready(function() {
+			$('table.streamtable').DataTable(
+			{
+			// Translate Datatables
+			language: {
+				"sEmptyTable":        "<?php echo trans('view.jQuery_sEmptyTable'); ?>",
+				"sInfo":              "<?php echo trans('view.jQuery_sInfo'); ?>",
+				"sInfoEmpty":         "<?php echo trans('view.jQuery_sInfoEmpty'); ?>",
+				"sInfoFiltered":      "<?php echo trans('view.jQuery_sInfoFiltered'); ?>",
+				"sInfoPostFix":       "<?php echo trans('view.jQuery_sInfoPostFix'); ?>",
+				"sInfoThousands":     "<?php echo trans('view.jQuery_sInfoThousands'); ?>",
+				"sLengthMenu":        "<?php echo trans('view.jQuery_sLengthMenu'); ?>",
+				"sLoadingRecords":    "<?php echo trans('view.jQuery_sLoadingRecords'); ?>",
+				"sProcessing":        "<?php echo trans('view.jQuery_sProcessing'); ?>",
+				"sSearch":            "<?php echo trans('view.jQuery_sSearch'); ?>",
+				"sZeroRecords":       "<?php echo trans('view.jQuery_sZeroRecords'); ?>",
+				"oPaginate": {
+					"sFirst":         "<?php echo trans('view.jQuery_PaginatesFirst'); ?>",
+					"sPrevious":      "<?php echo trans('view.jQuery_PaginatesPrevious'); ?>",
+					"sNext":          "<?php echo trans('view.jQuery_PaginatesNext'); ?>",
+					"sLast":          "<?php echo trans('view.jQuery_PaginatesLast'); ?>"
+					},
+				"oAria": {
+					"sSortAscending": "<?php echo trans('view.jQuery_sLast'); ?>",
+					"sSortDescending":"<?php echo trans('view.jQuery_sLast'); ?>"
+					}
+			},
+			//auto resize the Table to fit the viewing device
+			responsive: {
+				details: {
+					type: 'column'
+				}
+			},
+			autoWidth: false,
+			paging: false,
+			info: false,
+			searching: false,
+			aoColumnDefs: [ {
+				className: 'control',
+				orderable: false,
+				targets:   [0]
+			} ]
+			});
+	});
+	</script>
+
 @stop
