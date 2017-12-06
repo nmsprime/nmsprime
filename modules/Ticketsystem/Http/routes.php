f1f762f8261b2331ae1b4b065530d7c84e81479f
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2017-12-06 17:17:46 +0100

    LAR156: Add TicketType MVC with Tree View

--- modules/Ticketsystem/Http/routes.php
+++ modules/Ticketsystem/Http/routes.php
@@ -3,6 +3,7 @@
 BaseRoute::group([], function() {
 
 	BaseRoute::resource('Ticket', 'Modules\Ticketsystem\Http\Controllers\TicketController');
+	BaseRoute::resource('TicketType', 'Modules\Ticketsystem\Http\Controllers\TicketTypeController');
 	BaseRoute::resource('Comment', 'Modules\Ticketsystem\Http\Controllers\CommentController');
 	BaseRoute::resource('Ticketsystem', 'Modules\Ticketsystem\Http\Controllers\TicketsystemController');
 	Route::post('Ticket/detach/{id}/{func}', ['as' => 'Ticket.detach', 'uses' => 'Modules\Ticketsystem\Http\Controllers\TicketController@detach']);
