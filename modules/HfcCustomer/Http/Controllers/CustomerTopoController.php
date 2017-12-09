1cd9316136fe2ea972d9a9a12ca56ac3b3570199
Ole Ernst <ole.ernst@roetzer-engineering.com>
2017-12-09 12:52:34 +0100

    fix wrong diagram being shown when choosing a different time span

--- modules/HfcCustomer/Http/Controllers/CustomerTopoController.php
+++ modules/HfcCustomer/Http/Controllers/CustomerTopoController.php
@@ -260,6 +260,7 @@ class CustomerTopoController extends NetElementController {
 				// Description Line per Modem
 				$descr = $modem->lastname.' - '.$modem->zip.', '.$modem->city.', '.$modem->street.' '.$modem->house_number.' - '.$modem->mac;
 				$dia['descr']  = \HTML::linkRoute('Modem.edit', $descr, $modem->id);
+				$dia['row'] = \Input::has('row') ? \Input::get('row') : 'us_snr';
 
 				// Add diagrams to monitoring array (goes directly to view)
 				$monitoring[$modem->id] = $dia;
