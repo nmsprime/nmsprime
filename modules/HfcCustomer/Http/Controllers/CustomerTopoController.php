97fd8878225b240ddd033799afe6e30a58947be8
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-04-09 09:09:57 +0200

    default diagram row is US-power not US-SNR

--- modules/HfcCustomer/Http/Controllers/CustomerTopoController.php
+++ modules/HfcCustomer/Http/Controllers/CustomerTopoController.php
@@ -289,7 +289,7 @@ class CustomerTopoController extends NetElementController {
 				// Description Line per Modem
 				$descr = $modem->lastname.' - '.$modem->zip.', '.$modem->city.', '.$modem->street.' '.$modem->house_number.' - '.$modem->mac;
 				$dia['descr']  = \HTML::linkRoute('Modem.edit', $descr, $modem->id);
-				$dia['row'] = \Input::has('row') ? \Input::get('row') : 'us_snr';
+				$dia['row'] = \Input::has('row') ? \Input::get('row') : 'us_pwr';
 
 				// Add diagrams to monitoring array (goes directly to view)
 				$monitoring[$modem->id] = $dia;
