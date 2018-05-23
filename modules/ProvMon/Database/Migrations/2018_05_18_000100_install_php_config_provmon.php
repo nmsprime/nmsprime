b58676a84abe377056b8c6df2c9bd3817dd0dabe
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-05-23 13:17:17 +0200

    move cacti part of install migration to provmon module

---
+++ modules/ProvMon/Database/Migrations/2018_05_18_000100_install_php_config_provmon.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class InstallPhpConfigProvmon extends BaseMigration {
+
+	protected $tablename = '';
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$file = '/etc/httpd/conf.d/cacti.conf';
+		$str = file_get_contents($file);
+		$str = preg_replace('/Require all granted$/m', "Require all granted\n\t\tDirectoryIndex index.php", $str);
+		file_put_contents($file, $str);
+		system('systemctl reload httpd');
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+	}
+
+}
