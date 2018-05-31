nmsprime/nmsprime

commit 76746d16a0382b9e7ce6e19e9f757135a00ab61a

Billing: Allow credit amount in product

modules/BillingBase/Entities/Item.php
@@ -69,7 +69,7 @@ class Item extends \BaseModel {
 		}
 
 		$count = $this->count && $this->count != 1 ? "$this->count x " : '';
-		$price = $this->credit_amount != 0 ? $this->credit_amount : $this->product->price;
+		$price = $this->credit_amount ? : $this->product->price;
 		$price = ' | '.round($price, 2).'€';
 
 		/* Evaluate Colours
@@ -418,10 +418,10 @@ class Item extends \BaseModel {
 		if (!$ratio)
 			return null;
 
-		$this->count = $this->count ? $this->count : 1;
+		$this->count = $this->count ? : 1;
 
-		$this->charge = $this->product->type == 'Credit' ?  (-1) * $this->credit_amount * $ratio : $this->product->price * $ratio * $this->count;
-		$this->ratio  = $ratio ? $ratio : 1;
+		$this->charge = $this->product->type == 'Credit' ?  (-1) * ($this->credit_amount ? : $this->product->price) * $ratio : $this->product->price * $ratio * $this->count;
+		$this->ratio  = $ratio ? : 1;
 		$this->invoice_description = $this->product->name.' '.$text;
 		$this->invoice_description .= $this->accounting_text ? ' - '.$this->accounting_text : '';
 

modules/BillingBase/Http/Controllers/ProductController.php
@@ -42,7 +42,7 @@ class ProductController extends \BaseController {
 			array('form_type' => 'text', 'name' => 'maturity', 'description' => 'Maturity', 'select' => 'Internet', 'help' => trans('helper.Product_maturity')), 		// Laufzeit, tarif life time
 			array('form_type' => 'text', 'name' => 'period_of_notice', 'description' => 'Period of Notice', 'select' => 'Internet', 'help' => trans('helper.Product_maturity')), 		// Kündigungsfrist
 			array('form_type' => 'select', 'name' => 'costcenter_id', 'description' => 'Cost Center (optional)', 'value' => $ccs),
-			array('form_type' => 'text', 'name' => 'price', 'description' => 'Price (Net)', 'select' => 'Internet Voip TV Device Other'),
+			array('form_type' => 'text', 'name' => 'price', 'description' => 'Price (Net)'),
 			array_merge(array('form_type' => 'checkbox', 'name' => 'tax', 'description' => 'with Tax calculation ?', 'select' => 'TV Credit'), $model->tax === null ? ['checked' => true, 'value' => 1] : []),
 			array('form_type' => 'text', 'name' => 'email_count', 'description' => 'No. of email addresses', 'select' => 'Internet', 'hidden' => 1),
 			array('form_type' => 'checkbox', 'name' => 'bundled_with_voip', 'description' => 'Bundled with VoIP product?', 'select' => 'Internet'),

commit aca409e83ebc4ea6714a3254f9632fb996cea8cc

Bugfix-Item: Fix bug introduced by commit 76746d16a038 - show product price again

modules/BillingBase/Entities/Item.php
@@ -68,8 +68,8 @@ class Item extends \BaseModel {
 			}
 		}
 
-		$count = $this->count && $this->count != 1 ? "$this->count x " : '';
-		$price = $this->credit_amount ? : $this->product->price;
+		$count = $this->count > 1 ? "$this->count x " : '';
+		$price = floatval($this->credit_amount) ? : $this->product->price;
 		$price = ' | '.round($price, 2).'€';
 
 		/* Evaluate Colours