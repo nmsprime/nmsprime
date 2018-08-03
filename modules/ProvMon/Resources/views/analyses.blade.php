022aa410813a93f7f7b28331804133dcb8f0022f
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2018-08-03 09:15:57 +0200

    Bugfix/small fixes christian (#177)
    * Update Permissions, when Role is updated
    
    * Hide View Shortcut if View All is set
    
    * Indendation and structure
    
    * Fix Sidebar minify Button visibility
    
    * Delete unused code
    
    * Fix Datatables in Analysis Page when panelposition is saved
    
    * Cleanup AbilityController and abilities blade to be more readable + improve generic
    
    * Fix Ability display bug
    
    * Fix Shortcut Click Bug

--- modules/ProvMon/Resources/views/analyses.blade.php
+++ modules/ProvMon/Resources/views/analyses.blade.php
@@ -32,8 +32,7 @@
 		</div>
 
 		<div class="tab-pane fade in" id="flood-ping">
-			<?php $route = \Route::getCurrentRoute()->getUri(); ?>
-					<form route="$route" method="POST">Type:
+					<form method="POST">Type:
 						<input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
 						<select class="select2 form-control m-b-20" name="flood_ping" style="width : 100 %">
 							<option value="1">low load: 500 packets of 56 Byte</option> {{-- needs approximately 5 sec --}}
@@ -268,9 +267,16 @@
 </script>
 
 <script language="javascript">
-	$(document).ready(function() {
-		$('table.streamtable').DataTable(
-		{
+	let targetPage = window.location.href;
+		targetPage = targetPage.split('?');
+		targetPage = targetPage[0];
+	let panelPositionData = localStorage.getItem(targetPage) ? targetPage : "{!! isset($view_header) ? $view_header : 'undefined'!!}";
+    let event = 'load';
+	if (panelPositionData)
+		event = 'localstorage-position-loaded';
+
+	$(window).on(event, function() {
+		$('table.streamtable').DataTable({
 		{{-- Translate Datatables Base --}}
 			@include('datatables.lang')
 		responsive: {
