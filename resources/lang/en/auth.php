75c19c47fa208c70422cc1ae22d0544335861a2e
Christian Schramm <christian.schramm@roetzer-engineering.com>
2018-08-10 16:24:23 +0200

    add auth language file

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+ */
+
+    'failed' => 'Sorry! We could not found your login data anywhere!',
+    'throttle' => 'Too many Login attempts. Please try again in :seconds seconds.',
+    'NoRoles' => 'Your Account does not have any roles. Please contact your administrator.'
+];
