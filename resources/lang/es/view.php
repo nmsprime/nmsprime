6f29cc971cb162125d46cbc1f9751fd3923219d5
Torsten Schmidt <schmto@roetzer-engineering.com>
2018-08-24 14:33:17 +0200

    New translations view.php (Spanish)

--- resources/lang/es/view.php
+++ resources/lang/es/view.php
@@ -3,27 +3,28 @@
 return [
 //SEARCH
 		'Search_EnterKeyword' 		=> 'Ingresar palabra clave',
-		'Search_MatchesFor'			=> 'Coincida con',
-		'Search_In'					=> 'en',
+		'Search_MatchesFor'			=> 'Coincide para',
+		'Search_In'					=> 'en la tabla',
 //jQuery
 		//Translations for this at https://datatables.net/plug-ins/i18n/
 		'jQuery_sEmptyTable'		=> 'No hay datos disponibles en la tabla',
-		'jQuery_sInfo'				=> 'Mostrando _INICIO_ a _FINAL_ de entradas _TOTALES_',
-		'jQuery_sInfoEmpty'			=> 'Mostrando 0 a 0 de 0 entradas',
-		'jQuery_sInfoFiltered'		=> '(filtrado de _MÁXIMAS_entradas totales)',
+		'Search_Search ...'			=> 'Ingrese un termino de busqueda',
+		'jQuery_sInfo'				=> 'Mostrando _START_ hasta _END_ de _TOTAL_ entradas',
+		'jQuery_sInfoEmpty'			=> 'Mostrando 0 hasta 0 de 0 entradas',
+		'jQuery_sInfoFiltered'		=> '(filtrado de _MAX_ entradas)',
 		'jQuery_sInfoPostFix'		=> '',
 		'jQuery_sInfoThousands'		=> ',',
-		'jQuery_sLengthMenu'		=> 'Mostrar las entradas del _MENÚ_',
-		'jQuery_sLoadingRecords'	=> 'Cargando...',
-		'jQuery_sProcessing'		=> 'Procesando...',
+		'jQuery_sLengthMenu'		=> 'Mostrar _MENU_ entradas',
+		'jQuery_sLoadingRecords'	=> 'Cargando ... ',
+		'jQuery_sProcessing'		=> 'Procesando ... ',
 		'jQuery_sSearch'			=> 'Buscar:',
-		'jQuery_sZeroRecords'		=> 'Ningún registro encontrado',
+		'jQuery_sZeroRecords'		=> 'No hay entradas disponibles',
 		'jQuery_PaginatesFirst'		=> 'Primero',
 		'jQuery_PaginatesPrevious'	=> 'Anterior',
-		'jQuery_PaginatesNext'		=> 'Próximo',
-		'jQuery_PaginatesLast'		=> 'Último',
-		'jQuery_sLast'				=> ': activar para ordenar las columnas de forma ascendente',
-		'jQuery_sLast'				=> ': activar para ordenar las columnas de forma descendente',
+		'jQuery_PaginatesNext'		=> 'Siguiente',
+		'jQuery_PaginatesLast'		=> 'Ultimo',
+		'jQuery_sLast'				=> ': activar para ordenar la columna descendentemente',
+		'jQuery_sLast'				=> ': activate to sort column descending',
 		'jQuery_All'				=> 'Todo',
 		'jQuery_Print'				=> 'Imprimir',
 		'jQuery_colvis'				=> 'Visibilidad de la columna',
@@ -48,98 +49,141 @@ return [
 		'Menu_Contracts'			=> 'Contratos',
 		'Menu_Modems'				=> 'Módems',
 		'Menu_Endpoints'			=> 'Extremos',
+		'Menu_Number Range'			=> 'Nummernkreis',
 		'Menu_Configfiles' 			=> 'Archivo de configuración',
 		'Menu_QoS' 					=> 'QoS',
 		'Menu_CMTS' 				=> 'CMTS',
 		'Menu_Ip-Pools' 			=> 'IP-Pools',
 		'Menu_MTAs' 				=> 'MTAs',
-		'Menu_Phonenumbers'			=> 'Números de teléfono',
-		'Menu_PhoneTariffs'			=> 'Tarifa telefonica',
-		'Menu_Envia orders'			=> 'ordenes envia TEL',
+		'Menu_Phonenumbers'			=> 'Numeros telefonicos',
+		'Menu_PhoneTariffs'			=> 'Tarifas telefefonicas',
+		'Menu_Envia orders'			=> 'envia TEL ordenes',
 		'Menu_Envia contracts'		=> 'envia TEL contratos',
 
 	//User Settings
-		'Menu_UserSettings'			=> 'Configuración del Usuario',
-		'Menu_UserGlobSettings' 	=> 'Configuración global',
-		'Menu_Logout'				=> 'Cerrar sesión',
-		'Menu_UserRoleSettings'		=> 'Funciones de usuario',
+		'Menu_UserSettings'			=> 'Configuraciones de usuario',
+		'Menu_UserGlobSettings' 	=> 'Conf. de usuarios (global)',
+		'Menu_Logout'				=> 'Cerrar sesion',
+		'Menu_UserRoleSettings'		=> 'Roles (usuarios)',
 
 //HEADER
+		'Menu_CDRs'					=> 'EVNs',
+		'Menu_Tickets'				=> 'Tickets',
+		'Menu_Comment'				=> 'Comentarios',
 	//General
-		'Header_GlobalSearch'		=> 'Búsqueda global',
-		'Header_Overview'			=> 'Información general',
-		'Header_Assigned'			=> 'Asignado',
+		'Header_GlobalSearch'		=> 'Busqueda global',
+		'Header_Overview'			=> 'Panorama',
+		'Header_Assigned'			=> 'Designar',
 		'Header_Create'				=> 'Crear',
 	//Module specific
-
+	//BillingBase
+		//Product List
+		'Header_Mpr'				=> 'Reglas de posicionamiento',
+		'Header_Modem Positioning Rule' => 'Reglas de posicionamiento de modem',
+		'Header_EditModem Positioning Rule' => 'Editar reglas de posicionamiento de modem',
+		'Header_MprGeopos'			=> 'Geoposicion para reglas de posicionamiento de modem',
+		'Header_Modem Positioning Rule Geoposition'	=> 'Geoposicion para reglas de posicionamiento de modem',
+		'Header_EditModem Positioning Rule Geoposition' => 'Editar geoposicion para reglas de posicionamiento de modem',
 	//Global
 		//Logs
 		'Header_Logs'				=> 'Registros',
-		'Header_EditLogs'			=> 'Editar registro',
+		'Header_EditLogs'			=> 'Editar registros',
 		'Header_Roles'				=> 'Funcion|Funciones',
 	//Billing Base
 		//Prduct Entry
-		'Header_Product Entry'		=> 'Entrada de Producto|Entradas de Productos',
+		'Header_Product Entry'		=> 'Productos',
+		'Header_Users'				=> 'Usuario|Usuarios',
+		'Header_EditUsers'			=> 'Editar usuarios',
 		'Header_EditProduct Entry'	=> 'Editar producto',
 		//SEPA Accounts
 		'Header_SEPA Account'		=> 'Cuenta SEPA|Cuentas SEPA', //Workaround decide which one to use
-		'Header_EditSEPA Account'	=> 'Editar Cuenta SEPA',
+		'Header_EditSEPA Account'	=> 'Editar cuenta SEPA',
 		//CostCenter
-		'Header_CostCenter'			=> 'Centro de Costos|Centros de Costos', //Workaround decide which one to use
-		'Header_Cost Center'		=> 'Centro de Costos|Centros de Costos',
-		'Header_EditCost Center'	=> 'Modificar Centro de Costos',
+		'Header_Global Configurations' => 'Configuracion del sistema',
+		'Header_Global Config' 		=> 'Configuracion global',
+		'Header_EditGlobal Config'	=> 'Editar config. global',
+		'Header_Billing Config' 	=> 'Base de facturacion',
+		'Header_EditBilling Config' => 'Editar base de facturacion',
+		'Header_ProvVoip'			=> 'VOIP',
+		'Header_EditProvVoip Config'=> 'Editar VOIP',
+		'Header_Ccc Config'			=> 'Control del cliente',
+		'Header_EditCcc Config'		=> 'Editar control del cliente',
+		'Header_Prov Base'			=> 'Abastecimiento',
+		'Header_EditProv Base Config'=> 'Editar abastecimiento',
+		'Header_HfcBase'			=> 'HFC',
+		'Header_EditHfc Base Config'=> 'Editar HFC',
+		'Header_CostCenter'			=> 'Centro de costos', //Workaround decide which one to use
+		'Header_Cost Center'		=> 'Centro de costos',
+		'Header_EditCost Center'	=> 'Editar centro de costos',
 		//Company
-		'Header_EditCompany'		=> 'Modificar Empresa',
+		'Header_EditCompany'		=> 'Editar compa&ntilde;ia',
+		'Ability_Custom Abilities' 	=> 'Custom Abilities',
+		'Ability_Authentication'		=> 'Authentication',
+		'Ability_GlobalConfig' 		=> 'GlobalConfig',
+		'Ability_Ccc'				=> 'Ccc',
+		'Ability_BillingBase' 		=> 'BillingBase',
+		'Ability_HFC' 				=> 'HFC',
+		'Ability_ProvBase' 			=> 'ProvBase',
+		'Ability_ProvVoip' 			=> 'ProvVoip',
+		'Ability_ProvVoipEnvia'		=> 'ProvVoipEnvia',
+		'Ability_VoipMon'			=> 'VoipMon',
 		//Salesman
-		'Header_EditSalesman'		=> 'Modificar Vendedor',
+		'Header_EditSalesman'		=> 'Editar vendedor',
 		//Items
-		'Header_Item'				=> 'Ítem|Ítems',
-		'Header_EditItem'			=> 'Editar ítem', //??
+		'Header_Item'				=> 'Articulo|Articulos',
+		'Header_EditItem'			=> 'Editar articulo', //??
 		//Numberrange
-		'Header_NumberRange'		=> 'Numberrange|Numberranges',
+		'Header_NumberRange'		=> 'Rango numerico',
 	//SNMP Modul
 		//Device
 		'Header_Device'				=> 'Dispositivo|Dispositivos',
 		'Header_EditDevice'			=> 'Editar dispositivo',
 		//Device Type
-		'Header_EditDevice Type'	=> 'Editar dispositivo',
+		'Header_EditDevice Type'	=> 'Editar tipo de dispositivo',
+		'Header_Company'			=> 'Compa&ntilde;ias',
 	//Provisioning
 		//Contract
+		'Header_Salesman'			=> 'Vendedores',
 		'Header_Contract'			=> 'Contrato|Contratos',
 		'Header_EditContract'		=> 'Editar contrato',
-		'Header_SepaMandate'		=> 'SEPA-Mandate|SEPA-Mandates',
+		'Header_SepaMandate'		=> 'Orden SEPA|Ordenes SEPA',
 		//Modems
 		'Header_Modems'				=> 'Modem|Modems', //workaround
-		'Header_EditModems'			=> 'Editar Modem',
+		'Header_Invoice' 			=> 'Facturas',
+		'Header_EditModems'			=> 'Editar modem',
 		'Header_Modem'				=> 'Modem|Modems',
-		'Header_EditModem'			=> 'Editar Modem',
+		'Header_SEPA Mandate' 		=> 'Mandatos-SEPA',
+		'Header_EditModem'			=> 'Editar modem',
+		'Header_Settlement Run'		=> 'Procesos de liquidacion',
+		'Header_EditSettlement Run' => 'Editar procesos de liquidacion',
 		//Endpoint
-		'Header_Endpoints'			=> 'Endpoint|Endpoints',
-		'Header_EditEndpoints'		=> 'Edit Endpoints',
+		'Header_Endpoints'			=> 'Puntos finales',
+		'Header_EditEndpoints'		=> 'Editar puntos finales',
 		//Configfiles
-		'Header_Configfiles'		=> 'Configfile|Configfiles',
-		'Header_EditConfigfiles'	=> 'Edit Configfile',
+		'Header_Configfiles'		=> 'Archivos de config.',
+		'Header_EditConfigfiles'	=> 'Editar a. de config.',
 		//QoS
-		'Header_QoS'				=> 'QoS-Rule|QoS-Rules',
-		'Header_EditQoS'			=> 'Edit QoS-Rule',
+		'Header_QoS'				=> 'Reglas-QoS',
+		'Header_Device Type'		=> 'Tipo de dispositivo',
+		'Header_EditQoS'			=> 'Editar reglas-QoS',
 		//CMTS
 		'Header_CMTS'				=> 'CMTS|CMTSs',
-		'Header_EditCMTS'			=> 'Edit CMTS',
-		'Header_Config'				=> 'Config proposal|Config proposals',
+		'Header_EditCMTS'			=> 'Editar CMTS',
+		'Header_Config'				=> 'Config. propuesta|Config. propuestas',
 		//IpPool
 		'Header_IpPool'				=> 'IP-Pool|IP-Pools',
-		'Header_EditIpPool'			=> 'Edit IP-Pool',
+		'Header_EditIpPool'			=> 'Editar IP-Pool',
 		'Header_IP-Pools'			=> 'IP-Pool|IP-Pools',
-		'Header_EditIP-Pools'		=> 'Edit IP-Pool',
+		'Header_EditIP-Pools'		=> 'Editar IP-Pool',
 		// Tickets
 		'Header_Ticket'				=> 'Ticket|Tickets',
-		'Header_EditTicket'			=> 'Edit Ticket',
+		'Header_EditTicket'			=> 'Editar Ticket',
 	//HFC
 		//Topography
-		'Header_Topography - Modems'=> 'Topography - Modems',
+		'Header_Topography - Modems'=> 'Topografia - Modems',
 		'navigate'					=> 'Navegar',
 		'draw box'					=> 'Dibujar caja',
-		'draw polygon'				=> 'Dibujar polígono',
+		'draw polygon'				=> 'Dibujar poligono',
 		'modify'					=> 'Modificar elementos',
 	//VOIP
 		//MTA
@@ -148,15 +192,19 @@ return [
 		'Header_MTAs'				=> '|los MTA MTA',
 		'Header_EditMTAs'			=> 'Editar MTA',
 		//Phonenumber
-		'Header_Phonenumber'		=> 'Número de teléfono|Phonenumbers',
-		'Header_EditPhonenumber'	=> 'Editar número de teléfono',
-		'Header_Phonenumbers'		=> 'Número de teléfono|Phonenumbers',
-		'Header_EditPhonenumbers'	=> 'Editar número de teléfono',
+		'Header_Phonenumber'		=> 'Numero telefonico',
+		'Header_EditPhonenumber'	=> 'Editar numero telefonico',
+		'Header_Phonenumbers'		=> 'Numeros telefonicos',
+		'Header_EditPhonenumbers'	=> 'Editar numeros telefonicos',
+		'Header_Tickets'			=> 'Ticket|Tickets',
+		'Header_EditTickets'		=> 'Edit Tickets',
 		//Phone tariff
-		'Header_Phone tariffs'		=> 'Tarifa telefonica|Phonetariffs',
-		'Header_EditPhone tariffs'	=> 'Editar tarifa',
+		'Header_Comment'			=> 'Comentarios',
+		'Header_EditComment'		=> 'Editar comentarios',
+		'Header_Phone tariffs'		=> 'Tarifa telefonica|Tarifas telefonicas',
+		'Header_EditPhone tariffs'	=> 'Editar tarifas telefonicas',
 	//ProvVoipEnvia
-		'Header_EnviaOrders'		=> 'envia TEL orden|envia TEL órdenes',
+		'Header_EnviaOrders'		=> 'envia TEL orden|envia TEL ordenes',
 		'Header_EnviaContracts'		=> 'envia TEL contrato|envia TEL contratos',
 
 	//Header Relation
@@ -167,94 +215,119 @@ return [
 		// 'Edit'						=> 'Ändere ',
 
 //BUTTON
-		'Sign me in'				=> 'Usuario',
+		'Sign me in'				=> 'Iniciar sesion',
 		'Button_Create'				=> 'Crear',
-		'Button_Delete'				=> 'Eliminar elementos marcados',
+		'Button_Delete'				=> 'Borrar elementos marcados',
 		'Button_Force Restart'		=> 'Forzar reinicio',
 		'Button_Save'				=> 'Guardar',
-		'Button_Save / Restart'		=> 'Guardar / reiniciar',
+		'Button_Save / Restart'		=> 'Guardar / Reiniciar',
 		'Button_manage'				=> 'Gestión permite o prohíbe hacer todo lo posible con cada elemento. Este botón es un acceso directo para administrar todas las entidades de este módulo.',
+		'Header_Tree Table' 		=> 'Tabla de arbol',
+		'Header_EditTree Table'		=> 'Editar tabla de arbol',
 		'Button_view'				=> 'Acceso directo para la capacidad de ver todas las páginas de este módulo. Visualización es la capacidad básica que se requiere para todas las otras acciones dentro de la GUI.',
 		'Button_create'				=> 'Acceso directo para la capacidad de crear todas las páginas de este módulo.',
 		'Button_update'				=> 'Acceso directo para la capacidad de crear todas las páginas de este módulo.',
 		'Button_delete'				=> 'Acceso directo para la capacidad de crear todas las páginas de este módulo.',
-	//BillingBase
-		//Product List
-		'Button_Create Product Entry'	=> 'Crear entrada de producto',
-		//SEPA-Konto
-		'Button_Create SEPA Account'	=> 'Crear cuenta SEPA', //Workaround decide which one to use
-		'Button_Create SepaAccount'		=> 'Crear cuenta SEPA',
-
+		'Button_Create Product Entry'	=> 'Crear producto',
 		//CostCenter
-		'Button_Create Cost Center' 	=> 'Crear centro de costos', //Workaround decide which one to use
-		'Button_Create CostCenter' 		=> 'Crear centro de costos',
+		'Button_Create Cost Center' 	=> 'Crear centro costos', //Workaround decide which one to use
+		'Button_Create CostCenter' 		=> 'Crear centro costos',
+		'Button_Create Users'			=> 'Crear usuario',
 		//Company
-		'Button_Create Company'			=> 'Crear una empresa',
+		'Button_Create Company'			=> 'Crear compa&ntilde;ia',
 		//Salesman
 		'Button_Create Salesman'		=> 'Crear vendedor',
 		//Item
-		'Button_Create Item'			=> 'Crear artículo',
-		'sr_dl_logs' 					=> 'Descargar archivo de registro de todo',
+		'Button_Create Item'			=> 'Crear articulo',
+		'sr_dl_logs' 					=> 'Descargar logfile',
 		//Numberrange
-		'Button_Create NumberRange'		=> 'Crear rango de numeros',
+		'Button_Create NumberRange'		=> 'Crear rango numerico',
 
 	//SNMP Modul
 		//Device
 		'Button_Create Device'			=> 'Crear dispositivo',
 		//Device Type
 		'Button_Create Device Type'		=> 'Crear tipo de dispositivo',
+		'Button_Create Phonenumber'		=> 'Crear numero telefonico',
+		'Button_Create Tree Table'		=> 'Crear tabla arbol',
+		'Button_Create Modem Positioning Rule' => 'Crear regla de posicionamiento de modem',
+		'Button_Create Mpr'				=> 'Crear MPR',
+		'Button_Create Modem Positioning Rule Geoposition' => 'Crear regla de geoposicionamiento de modem',
+		'Button_Create MprGeopos'		=> 'Crear geoposicionamiento MPR',
+		'Button_Create Tickets'		=> 'Crear ticket',
+
+		'Button_Create Comment'		=> 'Crear comentario',
+		//SEPA-Konto
+		'Assigned'  				=> 'Zugewiesene',
+		'Button_Create SEPA Account'	=> 'Crear cuenta SEPA', //Workaround decide which one to use
+		'SEPA Account' 				=> 'Cuenta-SEPA',
+		'Create'					=> 'Crear',
+		'Edit'						=> 'Editar',
+		'Button_Create SepaAccount'		=> 'Crear cuenta SEPA',
 
 	//Provisioning
 		//Contract
-		// 'Button_Create Contract'		=> 'Neuer Vertrag',
-		// 'Button_Create SepaMandate'		=> 'Neues SEPA-Mandat',
+		'Button_Create Contract'		=> 'Crear contrato',
+		'Button_Create SepaMandate'		=> 'Crear mandado SEPA',
 		// //Modems
-		// 'Button_Create Modem'			=> 'Neues Modem',
+		'Button_Create Modem'			=> 'Crear modem',
 		// //Endpoints
-		// 'Button_Create Endpoints'		=> 'Neuer Endpunkt',
-		// //Configfiles
-		// 'Button_Create Configfiles'		=> 'Neue Konfigurationsdatei',
-		// //QoS
-		// 'Button_Create QoS'				=> 'Neue QoS-Regel',
-		// //CMTS
-		// 'Button_Create CMTS'			=> 'Neue CMTS',
-		// //IpPool
-		// 'Button_Create IpPool'			=> 'Neuer IP-Bereich', //workaround
-		// 'Button_Create IP-Pools'		=> 'Neuer IP-Bereich',
-
-
-	//VOIP
+		'Button_Create Endpoints'		=> 'Crear punto final',
 		//MTA
-		// 'Button_Create Mta'				=> 'Neues MTA',
+		'Button_Create Mta'				=> 'Crear MTA',
 		// //Phonenumber
-		// 'Button_Create Phonenumber'		=> 'Neue Telefonnummer',
-		// //Phone tariff
-		'Button_Create Phone tariffs'	=> 'Crear Tarifa',
+		'Button_Create Phone tariffs'	=> 'Crear tarifa telefonica',
+		// //Configfiles
+		'Button_Create Configfiles'		=> 'Crear archivos de configuracion',
+		// //QoS
+		'Button_Create Settlement Run'	=> 'Crear proceso de liquidacion',
+		'Button_Rerun Accounting Command' => 'Rerun Accounting Command',
+		'Button_Create QoS'				=> 'Crear regla QoS',
+		// //CMTS
+		'Button_Create CMTS'			=> 'Crear CMTS',
+		// //IpPool
+		'Button_Create IpPool'			=> 'Crear IP-Pool', //workaround
+		'Button_Create IP-Pools'		=> 'Crear IP-Pools',
 
-//
 // DASHBOARD
-//
 	'Dashboard_Contracts'			=> 'CONTRATOS ACTIVOS',
-	'Dashboard_ContractAnalytics'	=> 'Análisis del contrato (últimos 12 meses)',
-	'Dashboard_NoContracts'			=> 'No hay contratos disponibles.',
-	'Dashboard_Income'				=> 'INGRESO',
-	'Dashboard_IncomeAnalytics'		=> 'Detalle de Ingresos',
-	'Dashboard_Date'				=> 'Fecha',
-	'Dashboard_LinkDetails'			=> 'Ver Detalles',
+	'Dashboard_ContractAnalytics'	=> 'Estadisticas contrato (Ultimos 12 meses)',
+	'Dashboard_NoContracts'			=> 'No hay contratos disponibles',
+	'Dashboard_Income'				=> 'GANANCIAS',
+	'Dashboard_IncomeAnalytics'		=> 'Detalles de ganancias',
+	'Dashboard_Date'				=> 'FECHA',
+	'Dashboard_LinkDetails'			=> 'Ver detalles',
 	'Dashboard_Other'				=> 'Otros',
-	'Dashboard_Tickets' 			=> 'NUEVO TICKET',
-	'Dashboard_NoTickets' 			=> 'No hay nuevos Tickets.',
+	'Dashboard_Tickets' 			=> 'NUEVOS TICKETS',
+	'Dashboard_NoTickets' 			=> 'No hay tickets nuevos.',
 	'Dashboard_Quickstart' 			=> 'INICIO RAPIDO',
 
 //
 // Numberrange
 //
 	//Type
-	'Numberrange_Type_contract' => 'Contacto',
+	'Numberrange_Type_contract' => 'Contrato',
 	'Numberrange_Type_invoice' => 'Factura',
 
 //
 // Contract
+	'Dashboard_Net Income'			=> 'NETTOERLÖSE',
 //
-	'Contract_Numberrange_Failure' => 'No hay contrato libre para el centro de costos seleccionado!'
+	'Contract_Numberrange_Failure' => 'No se encuentra disponible el numero de contrato libre para el centro de costos seleccionado!',
+
+	'Ticket_State_New' => 'Nuevo',
+	'Ticket_State_In Process' => 'En progreso',
+	'Ticket_State_Closed' => 'Cerrado',
+	'Ticket_Type_General' => 'General',
+	'Ticket_Type_Technical' => 'Tecnico',
+	'Ticket_Type_Accounting' => 'Facturacion',
+	'Ticket_Priority_Trivial' => 'Bajo',
+	'Ticket_Priority_Minor' => 'Medio',
+	'Ticket_Priority_Major' => 'Alto',
+	'Ticket_Priority_Critical' => 'Crítico',
+	'Numberrange_Start' => 'Inicio',
+	'Numberrange_End' => 'Fin',
+	'Numberrange_Suffix' => 'Sufijo',
+	'Numberrange_Prefix' => 'Prefijo',
+	'Numberrange_Type' => 'Tipo',
 ];
