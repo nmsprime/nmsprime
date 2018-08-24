56936efcecd183933c01fae731faba53c055dce0
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2018-08-24 09:11:31 +0200

    Bugfix-Migration: Dont try to remove unique key when it's not there

--- modules/Ccc/Database/Migrations/2018_01_31_000100_update_CccAuthuserUndoUnique.php
+++ modules/Ccc/Database/Migrations/2018_01_31_000100_update_CccAuthuserUndoUnique.php
@@ -20,6 +20,10 @@ class UpdateCccAuthuserUndoUnique extends BaseMigration {
 		// Get the Key name from Database to definitely remove the key successfully
 		// before the migration the key is called 'cccauthusers_login_name_unique' ... after rollback it's called 'cccauthuser_login_name_unique'
 		$ret = \DB::connection($this->connection)->select(\DB::raw('SHOW keys from '.$this->tablename.' where Column_name=\'login_name\''));
+
+		if (!$ret)
+			return;
+
 		$key = $ret[0]->Key_name;
 
 		Schema::connection($this->connection)->table($this->tablename, function(Blueprint $table) use ($key)
