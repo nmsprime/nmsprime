fa357c24617bd608d9ed67202d438ce92fb362f0
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-09-07 10:37:21 +0200

    add cmts packet analysis (unerrored, corrected, uncorrectables) and rerun template import

---
+++ modules/ProvMon/Database/Migrations/2018_09_06_000100_install_update_cisco_cmts_template.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class InstallUpdateCiscoCmtsTemplate extends BaseMigration
+{
+    protected $tablename = '';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        system('su -s /bin/bash -c "php /usr/share/cacti/cli/import_template.php --filename=/var/www/nmsprime/modules/ProvMon/Console/cacti/cacti_host_template_cisco_cmts.xml" apache');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
