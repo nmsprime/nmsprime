c4a38b5672f8b3971f2498a1cf8f3dd4ff53c27e
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2018-09-07 09:11:16 +0200

    New translations auth.php (German)

--- resources/lang/de/auth.php
+++ resources/lang/de/auth.php
@@ -17,4 +17,5 @@ return [
     'throttle' => 'Zu viele Login Versuche. Versuchen Sie es bitte in :seconds Sekunden erneut.',
     'NoRoles' => 'Ihrem Account wurde(n) noch keine Rolle(n) zugewiesen. Bitte Kontaktieren Sie Ihren Administrator.',
 
+    'EnviaOrderDocument' => 'Zugriff zu EnviaOrder Dokument ist nicht erlaubt.\\nIhnen fehlt eventuell die Berechtigung für ProvVoipEnvia - Bitte wenden Sie sich an Ihren Administrator.',
 ];
