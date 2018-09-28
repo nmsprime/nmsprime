68f3e3e5b7d03972ae2672cdd072c16535c35143
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2018-09-28 11:13:02 +0200

    Add settlementrun log messages help button

--- modules/BillingBase/Resources/views/SettlementRun/logs.blade.php
+++ modules/BillingBase/Resources/views/SettlementRun/logs.blade.php
@@ -1,3 +1,22 @@
+<div class='row'>
+    {{-- Download logfile button --}}
+    @DivOpen(3)
+        {{ Form::open(array('route' => ['SettlementRun.log_dl', $view_var->id], 'method' => 'get')) }}
+            {{ Form::submit(trans('view.sr_dl_logs') , ['style' => 'simple']) }}
+        {{ Form::close() }}
+    @DivClose()
+
+    {{-- Link for help messages --}}
+    @DivOpen(8)
+    @DivClose()
+    @DivOpen(1)
+        <div class="m-b-20" align='left'>
+            <a href="https://devel.roetzer-engineering.com/confluence/display/NMS/SettlementRun+Logs" target='_default' data-toggle="popover" data-container="body" data-trigger="hover" title="" data-placement="right"
+            data-content="{{trans('messages.log_msg_descr')}}" data-original-title=""><i class="fa fa-2x text-info p-t-5 fa-question-circle"></i></a>
+        </div>
+    @DivClose()
+</div>
+
 <div class="table-responsive">
     <table class="table streamtable table-bordered" width="100%">
     <thead>
@@ -9,12 +28,9 @@
         </tr>
     </thead>
     <tbody>
-        <div class="m-b-20" align='right'>
-            {{ Form::open(array('route' => ['SettlementRun.log_dl', $view_var->id], 'method' => 'get')) }}
-                {{ Form::submit(trans('view.sr_dl_logs') , ['style' => 'simple']) }}
-            {{ Form::close() }}
-        @DivClose()
+        <div class='row'>
 
+        {{-- Table with log entries --}}
         @if (isset($logs))
             @foreach($logs as $row)
                 <tr class="{{ $row['color'] }}">
