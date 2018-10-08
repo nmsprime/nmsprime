ad9495d011158fc0d9ab2a41ee24f4b159224c48
Christian Schramm <christian.schramm@roetzer-engineering.com>
2018-10-08 09:08:22 +0200

    Move PreEqualization Logic to Cacti Command

--- modules/ProvMon/Console/cacti/ss_docsis.php
+++ modules/ProvMon/Console/cacti/ss_docsis.php
@@ -238,3 +238,153 @@ function ss_docsis($hostname, $snmp_community)
 
     return trim($result);
 }
+
+function _threenibble($hexcall)
+{
+    $ret = [];
+    $counter = 0;
+
+    foreach ($hexcall as $hex) {
+        $counter++;
+        if ($counter < 49) {
+            $hex = str_split($hex, 1);
+            if (ctype_alpha($hex[1]) || $hex[1] > 7) {
+                $hex[0] = 'F';
+                $hex = implode('', $hex);
+                $hex = preg_replace('/[^0-9A-Fa-f]/', '', $hex);
+                $hex = strrev("$hex");
+                $dec = array_values(array_slice(unpack('s', pack('h*', "$hex")), -1))[0];
+                array_push($ret, $dec);
+            } else {
+                $hex[0] = 0;
+                $hex = implode('', $hex);
+                $hex = preg_replace('/[^0-9A-Fa-f]/', '', $hex);
+                $hex = strrev("$hex");
+                $dec = array_values(array_slice(unpack('s', pack('h*', "$hex")), -1))[0];
+                array_push($ret, $dec);
+            }
+        }
+    }
+
+    return $ret;
+}
+
+function _fournibble($hexcall)
+{
+    $ret = [];
+    $counter = 0;
+
+    foreach ($hexcall as $hex) {
+        $counter++;
+        if ($counter < 49) {
+            $hex = preg_replace('/[^0-9A-Fa-f]/', '', $hex);
+            $hex = strrev("$hex");
+            $dec = array_values(array_slice(unpack('s', pack('h*', "$hex")), -1))[0];
+            array_push($ret, $dec);
+        }
+    }
+
+    return $ret;
+}
+
+function _nePwr($decimal, $maintap)
+{
+    $pwr = [];
+    $ans = implode('', array_keys($decimal, max($decimal)));
+    if ($maintap == $ans) {
+        $a2 = $decimal[$maintap];
+        $b2 = $decimal[$maintap + 1];
+        foreach (array_chunk($decimal, 2) as $val) {
+            $a1 = $val[0];
+            $b1 = $val[1];
+            $pwr[] = ($a1 * $a2 - $b1 * $b2) / ($a2 ** 2 + $b2 ** 2);
+            $pwr[] = ($a2 * $b1 + $a1 * $b2) / ($a2 ** 2 + $b2 ** 2);
+        }
+    } else {
+        for ($i = 0; $i < 48; $i++) {
+            $pwr[] = 0;
+        }
+    }
+
+    return $pwr;
+}
+
+function _energy($pwr, $maintap, $energymain)
+{
+    $ene_db = [];
+        //calculating the magnitude
+    $pwr = array_chunk($pwr, 2);
+    foreach ($pwr as $val) {
+        $temp = 10 * log10($val[0] ** 2 + $val[1] ** 2);
+        if (!(is_finite($temp))) {
+            $temp = -100;
+        }
+        $ene_db[] = round($temp, 2);
+    }
+
+    return $ene_db;
+}
+
+function _tdr($ene, $energymain, $freq)
+{
+    if ($ene[$energymain] == -100) {
+        $tdr = 0;
+    } else {
+        // propgagtion speed in cable networks (87% speed of light)
+        $v = 0.87 * 299792458;
+        unset($ene[$energymain]);
+        $highest = array_keys($ene, max($ene));
+        $highest = implode('', $highest);
+        $tap_diff = abs($energymain - $highest);
+        // 0.8 - Roll-off of filter; /2 -> round-trip (back and forth)
+        $tdr = $v * $tap_diff / (0.8 * $freq) / 2;
+        $tdr = round($tdr, 1);
+    }
+
+    return $tdr;
+}
+
+function _fft($pwr)
+{
+    $rea = [];
+    $imag = [];
+    $pwr = array_chunk($pwr, 2);
+    foreach ($pwr as $val) {
+        $rea[] = $val[0];
+        $imag[] = $val[1];
+    }
+
+    for ($i = 0; $i < 104; $i++) {
+        array_push($rea, 0);
+        array_push($imag, 0);
+    }
+
+    for ($i = 0; $i < 248; $i++) {
+        array_push($rea, array_shift($rea));
+        array_push($imag, array_shift($imag));
+    }
+
+    require_once __DIR__. '/../../../../vendor/brokencube/fft/src/FFT.php';
+    $ans = Brokencube\FFT\FFT::run($rea, $imag);
+    ksort($ans[0]);
+    ksort($ans[1]);
+    for ($i = 0; $i < 64; $i++) {
+        array_push($ans[0], array_shift($ans[0]));
+        array_push($ans[1], array_shift($ans[1]));
+    }
+
+    $answer = array_map(function ($v1, $v2) {
+        return 20 * log10(sqrt($v1 ** 2 + $v2 ** 2));
+    }, $ans[0], $ans[1]);
+
+        // stores the maximum amplitude value of the fft waveform
+    $x = max($answer);
+    $y = abs(min($answer));
+    $maxamp = $x >= $y ? $x : $y;
+
+    if (!(is_finite($maxamp))) {
+        $maxamp = 0;
+    }
+
+    return [$answer, $maxamp];
+}
