a5fe9ec2fc184393a6c22c4934b1b2aaf12799ab
Ole Ernst <ole.ernst@roetzer-engineering.com>
2018-11-16 09:43:56 +0100

    remove now unused agc method in ProvMon module

--- modules/ProvMon/Http/Controllers/ProvMonController.php
+++ modules/ProvMon/Http/Controllers/ProvMonController.php
@@ -636,50 +636,6 @@ class ProvMonController extends \BaseController
     }
 
     /**
-     * Calculate and set "Actual RX Power" of CMTS
-     *
-     * @param cmts:	CMTS object
-     * @param com:	SNMP RW community
-     * @param us:	Upstream values
-     * @return: array[section][Fieldname][Values]
-     */
-    protected function _set_new_rx_power($cmts, $com, $us)
-    {
-        echo "$cmts->hostname\n";
-
-        $rx_pwr = [];
-        foreach (array_keys($us['Frequency MHz']) as $idx) {
-            // continue if rx power is not available or zero (i.e. no CM on the channel)
-            if ($us['Rx Power dBmV'][$idx] === 'n/a' || ! $us['SNR dB'][$idx]) {
-                continue;
-            }
-            // the reference SNR is 24 dB
-            $r = round($us['Rx Power dBmV'][$idx] + 24 - $us['SNR dB'][$idx]);
-            // minimum actual power is 0 dB
-            if ($r < 0) {
-                $r = 0;
-            }
-            // maximum actual power is 10 dB
-            if ($r > 10) {
-                $r = 10;
-            }
-
-            echo "$idx: $r\t(".$us['SNR dB'][$idx].")\n";
-            try {
-                snmpset($cmts->ip, $com, ".1.3.6.1.4.1.4491.2.1.20.1.25.1.2.$idx", 'i', 10 * $r);
-            } catch (\Exception $e) {
-                $out = "error while setting new exptected us power on CMTS $cmts->hostname ($idx: $r)\n";
-                echo $out;
-                \Log::error($out);
-            }
-
-            $rx_pwr[$idx] = $r;
-        }
-
-        return $rx_pwr;
-    }
-
-    /**
      * The CMTS Realtime Measurement Function
      * Fetches all realtime values from CMTS with SNMP
      *
@@ -688,7 +644,7 @@ class ProvMonController extends \BaseController
      * @param ctrl:	shall the RX power be controlled?
      * @return: array[section][Fieldname][Values]
      */
-    public function realtime_cmts($cmts, $com, $ctrl = false)
+    public function realtime_cmts($cmts, $com)
     {
         // Copy from SnmpController
         $this->snmp_def_mode();
@@ -743,10 +699,6 @@ class ProvMonController extends \BaseController
                     unset($us[$entry][$idx]);
                 }
             }
-        }
-
-        if ($ctrl) {
-            $us['Rx Power dBmV'] = $this->_set_new_rx_power($cmts, $cmts->get_rw_community(), $us);
         }
 
         $ret['System'] = $sys;
