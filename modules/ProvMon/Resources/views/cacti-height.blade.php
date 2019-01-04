8241a0de69bde24aafa5c66085d82a10c424de0c
Ole Ernst <ole.ernst@roetzer-engineering.com>
2019-01-04 14:59:33 +0100

    simplify javascript and load analysis page more quickly by always choosing one column in cacti

--- modules/ProvMon/Resources/views/cacti-height.blade.php
+++ modules/ProvMon/Resources/views/cacti-height.blade.php
@@ -1,17 +1,12 @@
 <script language="javascript">
-    function resizeIframe(obj, host_id) {
-        if ('cols' in localStorage) {
-            localStorage.removeItem('cols');
-            return;
-        }
-
-        localStorage['cols'] = 2;
-        if (window.innerWidth < 800) {
-            localStorage['cols'] = 1;
-        }
-
-        obj.src='/cacti/graph_view.php?action=preview&columns=' + localStorage['cols'] + '&host_id=' + host_id;
-
-        setTimeout(function() { obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px'; }, 3000);
+    function resizeIframe(obj) {
+        setTimeout(function() {
+            var height = obj.contentWindow.document.body.scrollHeight;
+            /* workaround ipad (safari?) bug */
+            if (height < 1000) {
+                height *= 15;
+            }
+            obj.style.height = height + 'px';
+        }, 5000);
     };
 </script>
