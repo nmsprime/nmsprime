888637b73b81be004f8a5b5aaef35766e5bb4ec8
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2019-04-25 17:59:41 +0200

    Add retries and Timeout to session

--- modules/ProvMon/Console/cacti/modempoller.c
+++ modules/ProvMon/Console/cacti/modempoller.c
@@ -26,7 +26,7 @@
 
 /* ---------- Global Variables ---------- */
 int active_hosts, num_rows;
-
+int requestRetries = 2, requestTimeout = 5000000;
 MYSQL_RES *result;
 
 /* ---------- Global Structures ---------- */
@@ -228,6 +228,8 @@ void asynchronous(void)
         struct snmp_session sess;
         snmp_sess_init(&sess); /* initialize session */
         sess.version = SNMP_VERSION_2c;
+        sess.retries = requestRetries;
+        sess.timeout = requestTimeout;
         sess.peername = strdup(currentHost[0]);
         sess.community = strdup(currentHost[1]);
         sess.community_len = strlen(sess.community);
