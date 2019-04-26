acb03d2b29e2e41db5db2562b7883e1351f9df98
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2019-04-26 10:53:09 +0200

    Extend OID structure to keep track of root tree
    Signed-off-by: Christian Schramm <cschra@hrz.tu-chemnitz.de>

--- modules/ProvMon/Console/cacti/modempoller.c
+++ modules/ProvMon/Console/cacti/modempoller.c
@@ -35,7 +35,9 @@ struct oid
 {
     const char *Name;
     oid Oid[MAX_OID_LEN];
+    oid root[MAX_OID_LEN];
     size_t OidLen;
+    size_t rootlen;
 } oids[] = {
     {"1.3.6.1.2.1.1.1"},                  // SysDescr
     {".1.3.6.1.2.1.10.127.1.2.2.1.3"},    // # US Power (2.0)
@@ -78,6 +80,13 @@ void initialize(void)
         if (!read_objid(op->Name, op->Oid, &op->OidLen))
         {
             snmp_perror("read_objid");
+            exit(1);
+        }
+
+        op->rootlen = MAX_OID_LEN;
+        if (snmp_parse_oid(op->Name, op->root, &op->rootlen) == NULL)
+        {
+            snmp_perror(op->Name);
             exit(1);
         }
         op++;
