7b40f4d035e64641836bc99f924cd0f0add813a0
Christian Schramm <christian.schramm@roetzer-engineering.com>
2019-07-15 14:58:55 +0200

    Modules: Dont defer main module ServiceProvider

--- modules/ProvMon/Providers/ProvMonServiceProvider.php
+++ modules/ProvMon/Providers/ProvMonServiceProvider.php
@@ -11,7 +11,7 @@ class ProvMonServiceProvider extends ServiceProvider
      *
      * @var bool
      */
-    protected $defer = true;
+    protected $defer = false;
 
     /**
      * The artisan commands provided by this module
