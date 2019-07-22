4ead149d1f5f4c588ea75f9438b3f6865cffbaa0
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-07-22 17:07:24 +0200

    Add MVC Node

---
+++ modules/PropertyManagement/Entities/Node.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Modules\PropertyManagement\Entities;
+
+class Node extends \BaseModel
+{
+    // The associated SQL table for this Model
+    public $table = 'node';
+
+    // Add your validation rules here
+    public static function rules($id = null)
+    {
+        return [
+            'name' => 'required',
+            'street' => 'required',
+            'house_nr' => 'required',
+            'zip' => 'required',
+            'city' => 'required',
+        ];
+    }
+
+    /**
+     * View related stuff
+     */
+
+    // Name of View
+    public static function view_headline()
+    {
+        return 'Node';
+    }
+
+    public static function view_icon()
+    {
+        return '<i class="fa fa-share-alt-square"></i>';
+        return '<i class="fa fa-caret-square-o-right"></i>';
+    }
+
+    // AJAX Index list function
+    // generates datatable content and classes for model
+    public function view_index_label()
+    {
+        $bsclass = $this->headend ? 'success' : 'info';
+
+        return ['table' => $this->table,
+                'index_header' => ['node.name', 'street', 'house_nr', 'zip', 'city', "$this->table.type", "$this->table.headend"],
+                'header' => $this->name,
+                'bsclass' => $bsclass,
+                // 'eager_loading' => ['contract'],
+                // 'edit' => ['contract.firstname' => 'getContractFirstname'],
+            ];
+    }
+
+    /**
+     * Relationships:
+     */
+    // public function contract()
+    // {
+    //     return $this->belongsTo(Contract::class);
+    // }
+}
