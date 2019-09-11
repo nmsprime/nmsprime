835309279c5b81d5ae1c6901317635830681f9d3
Ole Ernst <ole.ernst@roetzer-engineering.com>
2019-09-11 09:44:04 +0200

    update modem statistics from modempoller wrapper
    after all modems have been updated, otherwise race conditions will occur

--- modules/ProvMon/Install/files/modempoller-nmsprime.sh
+++ modules/ProvMon/Install/files/modempoller-nmsprime.sh
@@ -11,3 +11,4 @@ su -s /bin/bash -c '/usr/bin/php /usr/share/cacti/poller.php' apache
 auth=$(grep '^DB_DATABASE\|^DB_USERNAME\|^DB_PASSWORD' /etc/nmsprime/env/global.env | sort | cut -d'=' -f2 | xargs)
 read -r -a auths <<< "$auth"
 echo 'UPDATE modem SET us_pwr = 0, us_snr = 0, ds_pwr = 0, ds_snr = 0;' | cat - update.sql | mysql -u "${auths[2]}" --password="${auths[1]}" "${auths[0]}"
+echo '\Modules\Dashboard\Http\Controllers\DashboardController::save_modem_statistics();' | php /var/www/nmsprime/artisan tinker
