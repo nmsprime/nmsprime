a716416972551d6cefd1181da7830b69c4eda70b
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-10-02 14:50:33 +0200

    Intendation commit

--- modules/PropertyManagement/Config/config.php
+++ modules/PropertyManagement/Config/config.php
@@ -6,18 +6,18 @@ return [
     'name' => 'PropertyManagement',
     'MenuItems' => [
         'Apartment' => [
-            'link'	=> 'Apartment.index',
-            'icon'	=> 'fa-bed',
+            'link'  => 'Apartment.index',
+            'icon'  => 'fa-bed',
             'class' => Apartment::class,
         ],
         'Node' => [
-            'link'	=> 'Node.index',
-            'icon'	=> 'fa-share-alt-square',
+            'link'  => 'Node.index',
+            'icon'  => 'fa-share-alt-square',
             'class' => Node::class,
         ],
         'Realty' => [
-            'link'	=> 'Realty.index',
-            'icon'	=> 'fa-building-o',
+            'link'  => 'Realty.index',
+            'icon'  => 'fa-building-o',
             'class' => Realty::class,
         ],
     ],
