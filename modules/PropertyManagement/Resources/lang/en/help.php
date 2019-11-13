0fb886eb0cd6c3b7392632b041b8b8e4e34fb2c5
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2019-11-13 12:09:22 +0100

    Contract: Use consistent label in every place, Modem: add validation for contract_id when created via realty - multiple modems in a realty must belong to the same contract

--- modules/PropertyManagement/Resources/lang/en/help.php
+++ modules/PropertyManagement/Resources/lang/en/help.php
@@ -3,4 +3,7 @@
 return [
     'administration' => 'Is this an administration company or a local contact? Administration companies can be chosen under \'Administration\' on the realty page, local contacts under \'Local contact\'.',
     'apartmentList' => 'Each entry in the list has the following structure: realty address - apartment-number (floor)',
+    'modem' => [
+        'contract_id' => 'For a single family house a second modem must belong to the same contract.',
+    ],
 ];
