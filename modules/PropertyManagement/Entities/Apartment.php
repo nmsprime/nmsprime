e2c2930fc0f7b410fca9e2be4d9a7658811c09cb
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2020-03-23 18:13:45 +0100

    Add one-to-one relation between Apartment and NetElement Tap-Port

--- modules/PropertyManagement/Entities/Apartment.php
+++ modules/PropertyManagement/Entities/Apartment.php
@@ -63,6 +63,15 @@ class Apartment extends \BaseModel
             $ret['Edit']['Contract']['relation'] = $this->contracts;
         }
 
+        if (\Module::collections()->has('HfcReq')) {
+            $ret['Edit']['tap']['class'] = 'NetElement';
+            $ret['Edit']['tap']['relation'] = $this->netelement ? collect([$this->netelement]) : null;
+
+            if ($this->netelement) {
+                $ret['Edit']['tap']['options']['hide_create_button'] = true;
+            }
+        }
+
         if (\Module::collections()->has('Ticketsystem')) {
             $ret['Edit']['Ticket']['class'] = 'Ticket';
             $ret['Edit']['Ticket']['relation'] = $this->tickets;
@@ -97,6 +106,11 @@ class Apartment extends \BaseModel
     public function realty()
     {
         return $this->belongsTo(Realty::class);
+    }
+
+    public function netelement()
+    {
+        return $this->hasOne(\Modules\HfcReq\Entities\NetElement::class);
     }
 
     public static function labelFromData($apartment)
