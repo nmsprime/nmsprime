e92e393791cfd21977a40ac0075a759814d142f7
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2020-04-02 15:02:55 +0200

    Intendation to spaces

--- modules/ProvMon/Resources/views/layouts/top.blade.php
+++ modules/ProvMon/Resources/views/layouts/top.blade.php
@@ -1,40 +1,40 @@
-	<?php
-		/**
-		 * Shows the html links of the related objects recursivly
-		 * TODO: should be placed in a global concept and not on module base
-		 */
-		$s = '';
+    <?php
+        /**
+         * Shows the html links of the related objects recursivly
+         * TODO: should be placed in a global concept and not on module base
+         */
+        $s = '';
 
-		$parent = $view_var;
-		$classname = explode('\\',get_class($parent));
-		$classname = end($classname);
+        $parent = $view_var;
+        $classname = explode('\\',get_class($parent));
+        $classname = end($classname);
 
-		while ($parent)
-		{
-			$tmp   = explode('\\',get_class($parent));
-			$view  = end($tmp);
-			$icon  = $parent->view_icon();
-			$label = is_array($ret = $parent->view_index_label()) ? $ret['header'] : $ret;
+        while ($parent)
+        {
+            $tmp   = explode('\\',get_class($parent));
+            $view  = end($tmp);
+            $icon  = $parent->view_icon();
+            $label = is_array($ret = $parent->view_index_label()) ? $ret['header'] : $ret;
 
-			$s = "<li>".HTML::decode(HTML::linkRoute($view.'.edit', $icon.$label, $parent->id)).'</li>'.$s;
+            $s = "<li>".HTML::decode(HTML::linkRoute($view.'.edit', $icon.$label, $parent->id)).'</li>'.$s;
 
-			$parent = $parent->view_belongs_to();
+            $parent = $parent->view_belongs_to();
 
-			if ($parent instanceof \Illuminate\Support\Collection) {
+            if ($parent instanceof \Illuminate\Support\Collection) {
                 $parent = $parent->first();
             }
-		}
+        }
 
-		// Show link to actual site. This depends on if we are in Modem Analyses or CPE Analyses context
-		if (!isset($type))
-		{
-			$route_ext = $classname == 'Modem' ? 'index' : 'netgw';
-			$s .= "<li class='nav-tabs'>".HTML::linkRoute("ProvMon.$route_ext", 'Analyses', $view_var->id).'</li>';
-		}
-		elseif ($type == 'CPE')
-			$s .= "<li class='nav-tabs'>".HTML::linkRoute('ProvMon.cpe', 'CPE Analysis', $view_var->id).'</li>';
-		elseif ($type == 'MTA')
-			$s .= "<li class='nav-tabs'>".HTML::linkRoute('ProvMon.mta', 'MTA Analysis', $view_var->id).'</li>';
+        // Show link to actual site. This depends on if we are in Modem Analyses or CPE Analyses context
+        if (!isset($type))
+        {
+            $route_ext = $classname == 'Modem' ? 'index' : 'netgw';
+            $s .= "<li class='nav-tabs'>".HTML::linkRoute("ProvMon.$route_ext", 'Analyses', $view_var->id).'</li>';
+        }
+        elseif ($type == 'CPE')
+            $s .= "<li class='nav-tabs'>".HTML::linkRoute('ProvMon.cpe', 'CPE Analysis', $view_var->id).'</li>';
+        elseif ($type == 'MTA')
+            $s .= "<li class='nav-tabs'>".HTML::linkRoute('ProvMon.mta', 'MTA Analysis', $view_var->id).'</li>';
 
-		echo "<li class='active'><a href='".route("$classname.index")."'><i class='fa fa-hdd-o'></i>$classname</a></li>".$s;
-	?>
+        echo "<li class='active'><a href='".route("$classname.index")."'><i class='fa fa-hdd-o'></i>$classname</a></li>".$s;
+    ?>
