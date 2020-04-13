5b0ceb82731bc794cedc935a07f60dbba7187bc1
Burim <arifi.burim@gmail.com>
2020-04-13 16:37:38 +0200

    Changes Vue components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,8 +11,12 @@ require('./bootstrap');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+window.axios = require('axios');
 
-Vue.component('example', require('./components/Example.vue'));
-Vue.component('test', require('./components/Test.vue'));
-Vue.component('ticket-message', require('./components/TicketPanel.vue'));
+Vue.config.productionTip = false;
+
+//
+// Vue.component('example', require('./components/Example.vue'));
+// Vue.component('test', require('./components/App.vue'));
+// Vue.component('ticket-message', require('./components/TicketPanel.vue'));
 
