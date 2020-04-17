48b31313131637b3fb2881c77d88b8e946e465f1
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2020-04-17 09:51:01 +0200

    New translations messages.php (Spanish)

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -94,6 +94,7 @@ return [
     'Headend'                   => 'Cabecera',
     'House number'              => 'Numero de la casa',
     'IBAN'                      => 'IBAN',
+    'indexDeleteDisabledTitle' => 'Can only be deleted when the object is not assigned to any :relation anymore.',
     'Internet Access'           => 'Acceso a Internet',
     'Inventar Number'           => 'Número de inventario',
     'Invoice'                   => 'Factura',
