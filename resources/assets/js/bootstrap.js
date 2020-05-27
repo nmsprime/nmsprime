e71ed8a4502191b663186ee54fb327bcea402551
Burim <arifi.burim@gmail.com>
2020-05-27 18:16:15 +0200

    Remove unnecessary components. Rollback to not using npm dependencies.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -6,7 +6,7 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-// window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 
 /**
