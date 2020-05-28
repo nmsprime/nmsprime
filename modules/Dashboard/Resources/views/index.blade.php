7994655b66547ccfe252032f89640d6f29f5a867
Christian Schramm <christian.schramm@roetzer-engineering.com>
2020-05-28 08:39:26 +0200

    TroubleDashboard: Restructure Main and Hfc Dashboard
    * Adjust Panel styling
    * change partition of panels on main Dashboard
    * Improve and unify quickstart styling
    * Add Icinga brand logo for link to Icinga

--- modules/Dashboard/Resources/views/index.blade.php
+++ modules/Dashboard/Resources/views/index.blade.php
@@ -1,98 +1,136 @@
 @extends ('Layout.default')
 
+@section ('contracts_total')
+<h4>{{ App\Http\Controllers\BaseViewController::translate_view('Contracts', 'Dashboard') }} {{ date('m/Y') }}</h4>
+<p style="overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
+    @if (isset($contracts_data['total']) && $contracts_data['total'])
+    {{ $contracts_data['total'] }}
+@else
+    {{ App\Http\Controllers\BaseViewController::translate_view('NoContracts', 'Dashboard') }}
+@endif
+</p>
+@stop
+
+@section ('income')
+<h4>{{ App\Http\Controllers\BaseViewController::translate_view('Net Income', 'Dashboard') }} {{ date('m/Y') }}</h4>
+<p style="overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
+    @if (isset($income_data['total']) && $income_data['total'])
+        {{ number_format($income_data['total'], 0, ',', '.') }}
+    @else
+        {{ number_format(0, 0, ',', '.') }}
+    @endif
+</p>
+@stop
+
 @section ('tickets')
-    <h4>{{ \App\Http\Controllers\BaseViewController::translate_view('Tickets', 'Dashboard') }}</h4>
-    <p>
-        @if ($tickets['total'])
-            {{ $tickets['total'] }}
-        @else
-            {{ \App\Http\Controllers\BaseViewController::translate_view('NoTickets', 'Dashboard') }}
-        @endif
+    <h4>{{ App\Http\Controllers\BaseViewController::translate_view('Tickets', 'Dashboard') }}</h4>
+    <p style="overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
+        {{ $tickets['total'] ?: App\Http\Controllers\BaseViewController::translate_view('NoTickets', 'Dashboard') }}
     </p>
 @stop
+
 @section ('date')
-    <h4>{{ \App\Http\Controllers\BaseViewController::translate_view('Date', 'Dashboard') }}</h4>
+    <h4>{{ App\Http\Controllers\BaseViewController::translate_view('Date', 'Dashboard') }}</h4>
     <p>{{ date('d.m.Y') }}</p>
 @stop
 
-@section('content')
-    @DivOpen(12)
-    <h1 class="page-header">{{ $title }}</h1>
-    @DivClose()
+@section ('contracts_total')
+<h4>{{ App\Http\Controllers\BaseViewController::translate_view('Contracts', 'Dashboard') }} {{ date('m/Y') }}</h4>
+<p style="overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">
+    @if (isset($contracts_data['total']) && $contracts_data['total'])
+        {{ $contracts_data['total'] }}
+    @else
+        {{ App\Http\Controllers\BaseViewController::translate_view('NoContracts', 'Dashboard') }}
+    @endif
+</p>
+@stop
 
-    @DivOpen(6)
-        <div class="row">
+@section('content')
+<h1 class="page-header h1">{{ $title }}</h1>
+<div class="row">
+    <div class="col-xs-12 col-sm-6 col-xl-3">
+        @include ('bootstrap.widget', [
+        'content' => 'contracts_total',
+            'widget_icon' => 'users',
+            'widget_bg_color' => 'green',
+            'link_target' => '#anchor-contracts',
+        ])
+    </div>
+    <div class="col-xs-12 col-sm-6 col-xl-3">
+        @include ('bootstrap.widget', [
+            'content' => 'income',
+            'widget_icon' => 'euro',
+            'widget_bg_color' => 'blue',
+            'link_target' => '#anchor-income',
+        ])
+    </div>
+    <div class="col-xs-12 col-sm-6 col-xl-4">
+        @include ('bootstrap.widget', [
+            'content' => 'tickets',
+            'widget_icon' => 'ticket',
+            'widget_bg_color' => 'orange',
+            'link_target' => '#anchor-tickets',
+        ])
+    </div>
+    <div class="col-xs-12 col-sm-6 col-xl-2">
+        @include ('bootstrap.widget',[
+            'content' => 'date',
+            'widget_icon' => 'calendar',
+            'widget_bg_color' => 'purple',
+            'linkText' => 'Heute'
+        ])
+    </div>
+</div>
+<div class="row">
+    <div class="col-xl-8 no-gutters ui-sortable">
+        @if($services)
+            @section ('trouble_dashboard')
+                @include('HfcBase::troubledashboard.panel')
+            @stop
+            @include ('bootstrap.panel', [
+                'content' => "trouble_dashboard",
+                'view_header' => 'Trouble Dashboard',
+                'height' => 'auto',
+                'i' => '5'
+            ])
+        @endif
+    </div>
+    <div class="col-xl-4 no-gutters ui-sortable">
+        @include('dashboard::widgets.quickstart')
+        @if ($tickets && $tickets['total'])
+            @section ('ticket_table')
+                @include('ticketsystem::panels.ticket_table')
+            @stop
+            @include ('bootstrap.panel', [
+                'content' => "ticket_table",
+                'view_header' => trans('messages.dashboard_ticket'),
+                'height' => 'auto',
+                'i' => '1',
+            ])
+        @endif
         @section ('dashboard_logs')
             @include('dashboard::timeline.logs')
         @stop
-        @include ('bootstrap.panel', ['content' => "dashboard_logs", 'view_header' => 'All updates', 'md' => 12, 'height' => 'auto', 'i' => '2'])
-        </div>
-        <div class="row">
-            @if ($tickets && $tickets['total'])
-                @section ('ticket_table')
-                    @include('ticketsystem::panels.ticket_table')
-                @stop
-                @include ('bootstrap.panel', [
-                    'content' => "ticket_table",
-                    'view_header' => trans('messages.dashboard_ticket'),
-                    'md' => 12, 'height' => 'auto', 'i' => '1',
-                ])
-            @endif
-        </div>
-    @DivClose()
-    @DivOpen(6)
-        <div class="row">
-            @DivOpen(6)
-                @include ('bootstrap.widget', [
-                    'content' => 'tickets',
-                    'widget_icon' => 'ticket',
-                    'widget_bg_color' => 'orange',
-                    'link_target' => '#anchor-tickets',
-                ])
-            @DivClose()
-            @DivOpen(6)
-                @include ('bootstrap.widget', [
-                    'content' => 'date',
-                    'widget_icon' => 'calendar',
-                    'widget_bg_color' => 'purple',
-                ])
-            @DivClose()
-        </div>
-        <div class="row">
-            @DivOpen(12)
-                @include('dashboard::widgets.quickstart')
-            @DivClose()
-        </div>
-        <div class="row">
-            @if($services)
-                @section ('impaired_services')
-                    @include('HfcBase::panels.impaired_services')
-                @stop
-                @include ('bootstrap.panel', [
-                    'content' => "impaired_services",
-                    'view_header' => 'Impaired Services',
-                    'md' => 12, 'height' => 'auto', 'i' => '3',
-                ])
-            @endif
-        </div>
-        <div class="row">
-            @if($netelements)
-                @section ('impaired_netelements')
-                    @include('HfcBase::panels.impaired_netelements')
-                @stop
-                @include ('bootstrap.panel', [
-                    'content' => "impaired_netelements",
-                    'view_header' => 'Impaired Netelements',
-                    'md' => 12, 'height' => 'auto', 'i' => '4',
-                ])
-            @endif
-        </div>
-    @DivClose()
-
-    @if (isset($news) && $news)
-        @section ('news')
-            @include('dashboard::panels.news')
-        @stop
-        @include ('bootstrap.panel', ['content' => "news", 'view_header' => 'News', 'md' => 12, 'height' => '350px', 'i' => '5'])
-    @endif
+        @include ('bootstrap.panel', [
+            'content' => "dashboard_logs",
+            'view_header' => 'All updates',
+            'height' => 'auto',
+            'i' => '2'
+        ])
+    </div>
+</div>
+<div class="row">
+@if (isset($news) && $news)
+    @section ('news')
+        @include('dashboard::panels.news')
+    @stop
+    @include ('bootstrap.panel', [
+        'content' => "news",
+        'view_header' => 'News',
+        'height' => '350px',
+        'md' => 12,
+        'i' => '5'
+    ])
+@endif
+</div>
 @stop
