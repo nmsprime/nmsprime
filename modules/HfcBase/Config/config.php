64f049135d56ffdde6d2eef5397491373186082f
Christian Schramm <christian.schramm@roetzer-engineering.com>
2020-06-04 02:15:23 +0200

    TroubleDashboard: Add Icinga Api Credentials to config

--- modules/HfcBase/Config/config.php
+++ modules/HfcBase/Config/config.php
@@ -1,7 +1,14 @@
 <?php
 
-    return 		[
+    return [
         'name' => 'HfcBase',
         'link' => 'HfcBase.index',
         'MenuItems' => [],
+        'icinga' => [
+            'api' => [
+                'user' => env('ICINGA_API_USER', 'root'),
+                'password' => env('ICINGA_API_PASSWORD', 'icinga'),
+                'url' => env('ICINGA_API_URL', 'https://localhost:5665/v1/'),
+            ],
+        ],
     ];
