046a8abf3a86d39b559509f7be4dab19bb360c84
Nino Ryschawy <nino.ryschawy@roetzer-engineering.com>
2020-06-17 13:08:47 +0200

    Billing: Add default models on installation to let settlement run be successful

--- modules/BillingBase/Resources/lang/en/messages.php
+++ modules/BillingBase/Resources/lang/en/messages.php
@@ -6,6 +6,7 @@ return [
         'missingEvn' => 'Missing CDR of :provider.',
         'wrongArgument' => 'Wrong format of date option. Execute the command for last month. (Default case)',
     ],
+    'installationDefaultModel' => 'The inserted data appears on the invoices of your customers - this is a default added during the automatic installation.',
     'modemAddressToInvoice' => 'Only the address of one modem of the corresponding contract will be added to the invoice.',
     'settlementrun' => [
         'state' => [
