e05f7cc3742c4ccd4bb1f8847802068f6077257a
Burim Arifi <arifi.burim@gmail.com>
2020-07-25 00:32:55 +0000

    Apply fixes from StyleCI

--- modules/HfcBase/Http/Controllers/VicinityGraphController.php
+++ modules/HfcBase/Http/Controllers/VicinityGraphController.php
@@ -1,19 +1,16 @@
 <?php
 
-
 namespace Modules\HfcBase\Http\Controllers;
 
 use View;
-use Module;
 use App\Http\Controllers\BaseController;
 
 class VicinityGraphController extends BaseController
 {
-    public function showGraph(){
+    public function showGraph()
+    {
         $title = 'Vicinity Graph';
 
         return View::make('HfcBase::VicinityGraph.graph', $this->compact_prep_view(compact('title')));
-
     }
-
 }
