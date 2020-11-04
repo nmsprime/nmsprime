63a26fa8f9d38495426850c603647a09cfb15949
Ole Ernst <ole.ernst@nmsprime.com>
2020-11-04 09:02:27 +0100

    display additional PNM features in analysis

--- modules/ProvMon/Resources/lang/en/messages.php
+++ modules/ProvMon/Resources/lang/en/messages.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'analysis' => [
+        'feature' => 'Feature',
+        'value' => 'Value',
+    ],
     'spectrum' => [
         'processing' => 'The measurement is in progress. Please try to create the spectrum again after some seconds!',
     ],
