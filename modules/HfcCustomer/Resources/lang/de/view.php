2e6a33c6e9441cee7b6611427995a8cae46b707d
Ole Ernst <ole.ernst@nmsprime.com>
2020-11-11 13:52:17 +0100

    show more details in truck tooltip

--- modules/HfcCustomer/Resources/lang/de/view.php
+++ modules/HfcCustomer/Resources/lang/de/view.php
@@ -2,4 +2,8 @@
 
 return [
     'vicinityGraph' => 'Nachbarschaftsgraph',
+    'affectedModems' => 'Betroffene Modems: ',
+    'failureType' => 'Fehlertyp: ',
+    'lastSeen' => 'Zuletzt gesehen: ',
+    'viewDetails' => 'Detailansicht',
 ];
--- modules/HfcCustomer/Resources/lang/en/view.php
+++ modules/HfcCustomer/Resources/lang/en/view.php
@@ -2,4 +2,8 @@
 
 return [
     'vicinityGraph' => 'Vicinity Graph',
+    'affectedModems' => 'Affected modems: ',
+    'failureType' => 'Failure type: ',
+    'lastSeen' => 'Last seen: ',
+    'viewDetails' => 'View details',
 ];
